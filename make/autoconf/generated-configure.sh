289a5ed8241d038826d1bef39a6ff4075dc6689f
David Holmes <dholmes@openjdk.org>
2018-01-24 05:12:08 -0500

    Fix mis-generated file

--- make/autoconf/generated-configure.sh
+++ make/autoconf/generated-configure.sh
@@ -5191,7 +5191,7 @@ VS_SDK_PLATFORM_NAME_2013=
 #CUSTOM_AUTOCONF_INCLUDE
 
 # Do not change or remove the following line, it is needed for consistency checks:
-DATE_WHEN_GENERATED=1516693408
+DATE_WHEN_GENERATED=1516225089
 
 ###############################################################################
 #
