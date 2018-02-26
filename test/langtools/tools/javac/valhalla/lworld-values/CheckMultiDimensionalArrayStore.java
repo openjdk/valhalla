48e249061cb340c7a4ebfd6d5b20419891fcb060
Srikanth Adayapalam <sadayapalam@openjdk.org>
2018-02-26 14:08:16 +0530

    [lworld] Additional tests for null assignment behavior involving multidimensional arrays

---
+++ test/langtools/tools/javac/valhalla/lworld-values/CheckMultiDimensionalArrayStore.java
@@ -0,0 +1,19 @@
+/*
+ * @test /nodynamiccopyright/
+ * @summary Check null store into multidimensional array
+ * @compile/fail/ref=CheckMultiDimensionalArrayStore.out -XDrawDiagnostics -XDdev CheckMultiDimensionalArrayStore.java
+ */
+
+public class CheckMultiDimensionalArrayStore {
+    __ByValue final class V {
+        final int x = 10;
+        class Y {
+            V [][][] va = new V[][][] {{{ null }}};
+            V [][] va2 =  {{ null }};
+            void foo() {
+                va = new V[][][] {{{ null }}};
+                va[0][0][0] = null;
+            }
+        }
+    }
+}
