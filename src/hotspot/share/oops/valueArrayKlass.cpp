ba8301c38d0446e6acb5b01351b20d6f860b081f
Mandy Chung <mchung@openjdk.org>
2018-03-23 15:01:40 -0700

    Fix osx build failure due to missing #include header file

--- src/hotspot/share/oops/valueArrayKlass.cpp
+++ src/hotspot/share/oops/valueArrayKlass.cpp
@@ -37,6 +37,7 @@
 #include "oops/instanceKlass.hpp"
 #include "oops/klass.inline.hpp"
 #include "oops/objArrayKlass.hpp"
+#include "oops/objArrayOop.inline.hpp"
 #include "oops/oop.inline.hpp"
 #include "oops/arrayKlass.hpp"
 #include "oops/arrayOop.hpp"
