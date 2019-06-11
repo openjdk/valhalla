da24a8641b581b5e4401ca6295852cc3d435ba6e
Ioi Lam <iklam@openjdk.org>
2019-06-11 14:47:21 -0700

    8225623: [lworld][c1] java -Xcomp -version fails with IllegalMonitorStateException

--- src/hotspot/cpu/x86/c1_CodeStubs_x86.cpp
+++ src/hotspot/cpu/x86/c1_CodeStubs_x86.cpp
@@ -328,10 +328,12 @@ void MonitorEnterStub::emit_code(LIR_Assembler* ce) {
   __ bind(_entry);
   if (_throw_imse_stub != NULL) {
     // When we come here, _obj_reg has already been checked to be non-null.
+    const int is_value_mask = markOopDesc::always_locked_pattern;
     Register mark = _scratch_reg->as_register();
     __ movptr(mark, Address(_obj_reg->as_register(), oopDesc::mark_offset_in_bytes()));
-    __ testl(mark, markOopDesc::always_locked_pattern);
-    __ jcc(Assembler::notZero, *_throw_imse_stub->entry());
+    __ andptr(mark, is_value_mask);
+    __ cmpl(mark, is_value_mask);
+    __ jcc(Assembler::equal, *_throw_imse_stub->entry());
   }
   ce->store_parameter(_obj_reg->as_register(),  1);
   ce->store_parameter(_lock_reg->as_register(), 0);
