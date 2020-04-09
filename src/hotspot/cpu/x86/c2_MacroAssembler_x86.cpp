e19dca7da4f6e90a246265f09f6867cc3a4dd0d0
Tobias Hartmann <tobias.hartmann@oracle.com>
2020-04-09 14:16:09 +0200

    8242454: [lworld] TestLWorld::test63 fails with "no exception thrown"

--- src/hotspot/cpu/x86/c2_MacroAssembler_x86.cpp
+++ src/hotspot/cpu/x86/c2_MacroAssembler_x86.cpp
@@ -495,6 +495,10 @@ void C2_MacroAssembler::fast_lock(Register objReg, Register boxReg, Register tmp
 
   // Attempt stack-locking ...
   orptr (tmpReg, markWord::unlocked_value);
+  if (EnableValhalla && !UseBiasedLocking) {
+    // Mask always_locked bit such that we go to the slow path if object is a value type
+    andptr(tmpReg, ~((int) markWord::biased_lock_bit_in_place));
+  }
   movptr(Address(boxReg, 0), tmpReg);          // Anticipate successful CAS
   lock();
   cmpxchgptr(boxReg, Address(objReg, oopDesc::mark_offset_in_bytes()));      // Updates tmpReg
