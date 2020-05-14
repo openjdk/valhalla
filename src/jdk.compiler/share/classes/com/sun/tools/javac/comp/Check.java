122aa795a8e5400b22b6c63d9bfb796e5b8596b1
Srikanth Adayapalam <sadayapalam@openjdk.org>
2020-05-14 12:55:19 +0000

    8244796: [lworld] Javac does not compile test/hotspot/jtreg/runtime/valhalla/valuetypes/UnsafeTest.java anymore
    Reviewed-by: jlaskey

--- src/jdk.compiler/share/classes/com/sun/tools/javac/comp/Check.java
+++ src/jdk.compiler/share/classes/com/sun/tools/javac/comp/Check.java
@@ -898,9 +898,7 @@ public class Check {
     }
 
     void checkParameterizationWithValues(DiagnosticPosition pos, Type t) {
-        if (t.tsym != syms.classType.tsym) { // tolerate Value.class.
-            valueParameterizationChecker.visit(t, pos);
-        }
+        valueParameterizationChecker.visit(t, pos);
     }
 
     /** valueParameterizationChecker: A type visitor that descends down the given type looking for instances of value types
