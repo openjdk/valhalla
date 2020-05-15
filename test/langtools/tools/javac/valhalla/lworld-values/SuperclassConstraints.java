c82e6b9815e50ece6f474b86390f85d1e0f9ced8
Srikanth Adayapalam <sadayapalam@openjdk.org>
2020-05-15 05:52:43 +0000

    8244563: [lworld] Javac does not allow jlO to be express superclass of inline types.

--- test/langtools/tools/javac/valhalla/lworld-values/SuperclassConstraints.java
+++ test/langtools/tools/javac/valhalla/lworld-values/SuperclassConstraints.java
@@ -12,7 +12,7 @@ public class SuperclassConstraints {
     // Test that super class cannot be concrete, including express jlO
     static class BadSuper {}
     inline class I0 extends BadSuper {} // ERROR: concrete super class
-    inline class I1 extends Object {}   // ERROR: concrete jlO cannot be express-superclass
+    inline class I1 extends Object {}   // OK: concrete jlO can be express-superclass
     inline class I2 {} // OK
 
     // Test that abstract class is allowed to be super including when extending jlO
