55bfd32bf8d9a4d4c4a01a8a59c8de442ded23ad
Srikanth Adayapalam <sadayapalam@openjdk.org>
2020-06-16 11:34:02 +0000

    8244561: [lworld] Javac should not allow instantiation of V.ref or V.val

--- src/jdk.compiler/share/classes/com/sun/tools/javac/comp/Attr.java
+++ src/jdk.compiler/share/classes/com/sun/tools/javac/comp/Attr.java
@@ -2721,6 +2721,14 @@ public class Attr extends JCTree.Visitor {
             boolean isSpeculativeDiamondInferenceRound = TreeInfo.isDiamond(tree) &&
                     resultInfo.checkContext.deferredAttrContext().mode == DeferredAttr.AttrMode.SPECULATIVE;
             boolean skipNonDiamondPath = false;
+            // Check that it is an instantiation of a class and not a projection type
+            if (clazz.hasTag(SELECT)) {
+                JCFieldAccess fieldAccess = (JCFieldAccess) clazz;
+                if (fieldAccess.selected.type.isValue() &&
+                        (fieldAccess.name == names.ref || fieldAccess.name == names.val)) {
+                    log.error(tree.pos(), Errors.ProjectionCantBeInstantiated);
+                }
+            }
             // Check that class is not abstract
             if (cdef == null && !isSpeculativeDiamondInferenceRound && // class body may be nulled out in speculative tree copy
                 (clazztype.tsym.flags() & (ABSTRACT | INTERFACE)) != 0) {
