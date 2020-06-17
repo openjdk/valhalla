e57389a1db148e9fa86ead394098905147658575
David Simms <david.simms@oracle.com>
2020-06-17 12:20:21 +0200

    Version 16

--- src/java.base/share/classes/jdk/experimental/value/MethodHandleBuilder.java
+++ src/java.base/share/classes/jdk/experimental/value/MethodHandleBuilder.java
@@ -93,7 +93,7 @@ public class MethodHandleBuilder {
         IsolatedMethodBuilder isolatedMethodBuilder = new IsolatedMethodBuilder(className, lookup);
         isolatedMethodBuilder
                 .withSuperclass(Object.class)
-                .withMajorVersion(59)
+                .withMajorVersion(60)
                 .withMinorVersion(0)
                 .withFlags(Flag.ACC_PUBLIC)
                 .withMethod(methodName, type, M ->
