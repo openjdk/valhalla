2ffad6e51bfca9971d5a5fd2341847338c75c299
Frederic Parain <frederic.parain@oracle.com>
2020-07-02 11:54:46 -0400

    Add comment and fix copyright headers

--- src/hotspot/share/prims/jvmtiEnvBase.cpp
+++ src/hotspot/share/prims/jvmtiEnvBase.cpp
@@ -961,6 +961,9 @@ JvmtiEnvBase::get_object_monitor_usage(JavaThread* calling_thread, jobject objec
   uint32_t debug_bits = 0;
   // first derive the object's owner and entry_count (if any)
   {
+    // Inline types instances don't support synchronization operations
+    // they are marked as always locked and no attempt to remove a
+    // potential bias (which cannot exist) should be made
     if (!hobj()->mark().is_always_locked()) {
       // Revoke any biases before querying the mark word
       if (at_safepoint) {
