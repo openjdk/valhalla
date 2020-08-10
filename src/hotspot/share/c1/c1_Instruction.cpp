8d91e615beacf3d3a7aba444b7d339c16c9635a0
Frederic Parain <frederic.parain@oracle.com>
2020-08-10 11:12:51 -0400

    Fix method indentation

--- src/hotspot/share/c1/c1_Instruction.cpp
+++ src/hotspot/share/c1/c1_Instruction.cpp
@@ -423,34 +423,34 @@ StoreField::StoreField(Value obj, int offset, ciField* field, Value value, bool 
                        ValueStack* state_before, bool needs_patching)
   : AccessField(obj, offset, field, is_static, state_before, needs_patching)
   , _value(value)
-  {
-    set_flag(NeedsWriteBarrierFlag, as_ValueType(field_type())->is_object());
+{
+  set_flag(NeedsWriteBarrierFlag, as_ValueType(field_type())->is_object());
 #ifdef ASSERT
   AssertValues assert_value;
   values_do(&assert_value);
 #endif
-    pin();
-    if (value->as_NewInlineTypeInstance() != NULL) {
-      value->as_NewInlineTypeInstance()->set_not_larva_anymore();
-    }
+  pin();
+  if (value->as_NewInlineTypeInstance() != NULL) {
+    value->as_NewInlineTypeInstance()->set_not_larva_anymore();
   }
+}
 
 StoreIndexed::StoreIndexed(Value array, Value index, Value length, BasicType elt_type, Value value,
                            ValueStack* state_before, bool check_boolean, bool mismatched)
   : AccessIndexed(array, index, length, elt_type, state_before, mismatched)
   , _value(value), _check_boolean(check_boolean)
-  {
-    set_flag(NeedsWriteBarrierFlag, (as_ValueType(elt_type)->is_object()));
-    set_flag(NeedsStoreCheckFlag, (as_ValueType(elt_type)->is_object()));
+{
+  set_flag(NeedsWriteBarrierFlag, (as_ValueType(elt_type)->is_object()));
+  set_flag(NeedsStoreCheckFlag, (as_ValueType(elt_type)->is_object()));
 #ifdef ASSERT
   AssertValues assert_value;
   values_do(&assert_value);
 #endif
-    pin();
-    if (value->as_NewInlineTypeInstance() != NULL) {
-      value->as_NewInlineTypeInstance()->set_not_larva_anymore();
-    }
+  pin();
+  if (value->as_NewInlineTypeInstance() != NULL) {
+    value->as_NewInlineTypeInstance()->set_not_larva_anymore();
   }
+}
 
 
 // Implementation of Invoke
