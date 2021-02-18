ba95161fbeb21be1257c0c029a0772d1eb5967fa
David Simms <david.simms@oracle.com>
2021-02-18 11:44:37 +0100

    inline to primitive keyword changes

--- test/hotspot/jtreg/runtime/valhalla/TypeRestrictions/SimpleTest.java
+++ test/hotspot/jtreg/runtime/valhalla/TypeRestrictions/SimpleTest.java
@@ -34,7 +34,7 @@ import jdk.test.lib.Asserts;
  */
 public class SimpleTest {
 
-    static inline class Point {
+    static primitive class Point {
         public double x;
         public double y;
         public Point(double x, double y) { this.x = x; this.y = y; }
