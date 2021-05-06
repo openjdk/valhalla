b60a6625c3c891a0ac82ab77629a751372c74531
David Simms <david.simms@oracle.com>
2021-05-06 08:46:44 +0200

    Adjust CDS class list in test

--- test/hotspot/jtreg/runtime/cds/appcds/customLoader/OldClassAndInf.java
+++ test/hotspot/jtreg/runtime/cds/appcds/customLoader/OldClassAndInf.java
@@ -57,18 +57,20 @@ public class OldClassAndInf {
         String classlist[] = new String[] {
             "OldClassApp",
             "java/lang/Object id: 1",
-            "OldSuper id: 2 super: 1 source: " + loadeesJar,
-            "ChildOldSuper id: 3 super: 2 source: " + loadeesJar,
-            "GChild id: 4 super: 3 source: " + loadeesJar
+            "java/lang/IdentityObject id: 2",
+            "OldSuper id: 3 super: 1 interfaces: 2 source: " + loadeesJar,
+            "ChildOldSuper id: 4 super: 3 source: " + loadeesJar,
+            "GChild id: 5 super: 4 source: " + loadeesJar
         };
         doTest(classlist, loadeesJar, "true",  "OldSuper", "ChildOldSuper", "GChild");
 
         String classlist2[] = new String[] {
             "OldClassApp",
             "java/lang/Object id: 1",
-            "OldInf id: 2 super: 1 source: " + loadeesJar2,
-            "ChildOldInf id: 3 super: 1 interfaces: 2 source: " + loadeesJar2,
-            "GChild2 id: 4 super: 3 source: " + loadeesJar2
+            "java/lang/IdentityObject id: 2",
+            "OldInf id: 3 super: 1 source: " + loadeesJar2,
+            "ChildOldInf id: 4 super: 1 interfaces: 2 3 source: " + loadeesJar2,
+            "GChild2 id: 5 super: 4 source: " + loadeesJar2
         };
         doTest(classlist2, loadeesJar2, "true", "OldInf", "ChildOldInf", "GChild2");
     }
