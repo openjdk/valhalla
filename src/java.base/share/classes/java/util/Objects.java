bb1071183ffdce1c8ed4ca09790437d8543df831
Roger Riggs <rriggs@openjdk.org>
2021-06-23 13:05:00 +0000

    8269172: Add java.util.Objects.newIdentity method
    Reviewed-by: mchung

--- src/java.base/share/classes/java/util/Objects.java
+++ src/java.base/share/classes/java/util/Objects.java
@@ -424,20 +424,6 @@ public final class Objects {
     }
 
     /**
-     * Returns a new Object implementing the {@code IdentityObject} interface.
-     * The object is a unique {@link IdentityObject} suitable for all purposes
-     * for which {@code new Object{}} was used including synchronization,
-     * mutexes and unique placeholders.
-     *
-     * @return a new Object implementing the IdentityObject interface
-     * @since Valhalla
-     */
-    public static IdentityObject newIdentity() {
-        // Return a new instance of an anonymous inner class.
-        return new IdentityObject() { };
-    }
-
-    /**
      * Checks if the {@code index} is within the bounds of the range from
      * {@code 0} (inclusive) to {@code length} (exclusive).
      *
@@ -511,5 +497,16 @@ public final class Objects {
     public static
     long checkFromIndexSize(long fromIndex, long size, long length) {
         return Preconditions.checkFromIndexSize(fromIndex, size, length, null);
+    }
+    /**
+     * {@return a new instance of an unspecified class}
+     * The object has a unique identity; no other references to it exist.
+     * It can be used for synchronization, or where a placeholder Object is needed.
+     * Use this method to avoid relying on the {@linkplain Object#Object() Object constructor}.
+     *
+     * @since 17
+     */
+    public static Object newIdentity() {
+        return new Object() {};
     }
 }
