4c5ef8eebc8d71f96403eeb163b7a1c71b4aaaeb
Jesper Steen Møller <jesper@selskabet.org>
2021-09-23 00:04:49 +0200

    Updated test information and copyright.

--- test/langtools/tools/javac/records/RecordReading.java
+++ test/langtools/tools/javac/records/RecordReading.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2020, 2021, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -25,6 +25,7 @@
  * @test
  * @summary test the records can be read by javac properly
  * @library /tools/lib
+ * @bug 8273018
  * @modules jdk.compiler/com.sun.tools.javac.api
  *          jdk.compiler/com.sun.tools.javac.main
  * @build toolbox.ToolBox toolbox.JavacTask
