a9a8c83a4b2411149bb88b053827778d0e934c76
Srikanth Adayapalam <sadayapalam@openjdk.org>
2022-01-24 07:58:21 +0000

    8280514: [API-cleanup] Merge the internal API points Types.is{Primitive/Value}Class with Type.is{Primitive/Value}Class

--- src/jdk.compiler/share/classes/com/sun/tools/javac/comp/Check.java
+++ src/jdk.compiler/share/classes/com/sun/tools/javac/comp/Check.java
@@ -851,7 +851,7 @@ public class Check {
      *  @param primitiveClassOK       If false, a primitive class does not qualify
      */
     Type checkRefType(DiagnosticPosition pos, Type t, boolean primitiveClassOK) {
-        if (t.isReference() && (primitiveClassOK || !types.isPrimitiveClass(t)))
+        if (t.isReference() && (primitiveClassOK || !t.isPrimitiveClass()))
             return t;
         else
             return typeTagError(pos,
@@ -953,7 +953,7 @@ public class Check {
         @Override
         public Void visitClassType(ClassType t, DiagnosticPosition pos) {
             for (Type targ : t.allparams()) {
-                if (types.isPrimitiveClass(targ)) {
+                if (targ.isPrimitiveClass()) {
                     log.error(pos, Errors.GenericParameterizationWithPrimitiveClass(t));
                 }
                 visit(targ, pos);
@@ -1369,7 +1369,7 @@ public class Check {
                 mask = implicit = InterfaceVarFlags;
             else {
                 mask = VarFlags;
-                if (types.isValueClass(sym.owner.type) && (flags & STATIC) == 0) {
+                if (sym.owner.type.isValueClass() && (flags & STATIC) == 0) {
                     implicit |= FINAL;
                 }
             }
@@ -2506,7 +2506,7 @@ public class Check {
     }
         // where
         private boolean cyclePossible(VarSymbol symbol) {
-            return (symbol.flags() & STATIC) == 0 && types.isPrimitiveClass(symbol.type);
+            return (symbol.flags() & STATIC) == 0 && symbol.type.isPrimitiveClass();
         }
 
     void checkNonCyclicDecl(JCClassDecl tree) {
