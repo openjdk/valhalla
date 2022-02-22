e737fa11066e4820fc79392dee7854d9b9e9f415
Srikanth Adayapalam <srikanth.adayapalam@oracle.com>
2022-02-22 14:56:47 +0530

    8281323:  Unnecessary entries in Preload attribute

--- src/jdk.compiler/share/classes/com/sun/tools/javac/jvm/Gen.java
+++ src/jdk.compiler/share/classes/com/sun/tools/javac/jvm/Gen.java
@@ -1106,6 +1106,10 @@ public class Gen extends JCTree.Visitor {
             code.newLocal(v);
         }
         checkDimension(tree.pos(), v.type);
+        Type localType = v.erasure(types);
+        if (localType.requiresPreload(env.enclClass.sym)) {
+            poolWriter.enterPreloadClass((ClassSymbol) localType.tsym);
+        }
     }
 
     public void visitSkip(JCSkip tree) {
