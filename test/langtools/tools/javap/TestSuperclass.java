09786c6467fc6fe64d44bcd2574c3ea9d97f20c7
Srikanth Adayapalam <srikanth.adayapalam@oracle.com>
2022-05-12 14:01:49 +0530

    Fix failing tier1 langtools test: tools/javap/TestSuperclass.java

--- test/langtools/tools/javap/TestSuperclass.java
+++ test/langtools/tools/javap/TestSuperclass.java
@@ -158,7 +158,7 @@ public class TestSuperclass {
 
     class JavaSource extends SimpleJavaFileObject {
         static final String template =
-                  "#CK Test#GK #EK { }\n"
+                  "identity #CK Test#GK #EK { }\n"
                 + "#SK\n";
         final String source;
 
