61c28c2f49c3f7d80241f711d823d913718a8073
David Simms <dsimms@openjdk.org>
2022-09-08 12:03:02 +0000

    8293536: [lworld] serviceability/jvmti/RedefineClasses/RedefineObject.java fails with added ACC_IDENTITY to object for old version
    Reviewed-by: fparain

--- src/hotspot/share/classfile/classFileParser.cpp
+++ src/hotspot/share/classfile/classFileParser.cpp
@@ -6206,7 +6206,7 @@ void ClassFileParser::parse_stream(const ClassFileStream* const stream,
     if(!supports_inline_types()) {
       const bool is_module = (flags & JVM_ACC_MODULE) != 0;
       const bool is_interface = (flags & JVM_ACC_INTERFACE) != 0;
-      if (!is_module && !is_interface) {
+      if (!is_module && !is_interface && !is_java_lang_Object) {
         flags |= JVM_ACC_IDENTITY;
       }
     }
