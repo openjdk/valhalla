65921855e292d648a9589e45ba237463df5e4f83
Harold Seigel <harold.seigel@oracle.com>
2022-09-08 17:09:25 +0000

    8293134: Valhalla: Verifier error in method call with abstract value class arg

--- src/hotspot/share/classfile/verificationType.cpp
+++ src/hotspot/share/classfile/verificationType.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2003, 2021, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2003, 2022, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -64,7 +64,7 @@ bool VerificationType::resolve_and_check_assignability(InstanceKlass* klass, Sym
     }
   }
 
-  if (this_class->access_flags().is_value_class()) return false;
+  if (this_class->access_flags().is_primitive_class()) return false;
   if (this_class->is_interface() && (!from_field_is_protected ||
       from_name != vmSymbols::java_lang_Object())) {
     // If we are not trying to access a protected field or method in
