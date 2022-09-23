1b80935b5bce9f2126201c6bd5d487a8e686ffe2
Vicente Romero <vicente.romero@oracle.com>
2022-09-22 20:32:14 -0400

    fixing additional HS tests

--- test/hotspot/jtreg/runtime/valhalla/inlinetypes/TestInheritedInlineTypeFields.java
+++ test/hotspot/jtreg/runtime/valhalla/inlinetypes/TestInheritedInlineTypeFields.java
@@ -29,7 +29,7 @@ import jdk.test.lib.Asserts;
  * @test TestInheritedInlineTypeFields
  * @summary Test if inline field klasses are correctly retrieved for inherited fields
  * @library /test/lib
- * @compile -XDallowFlattenabilityModifiers Point.java TestInheritedInlineTypeFields.java
+ * @compile -XDallowFlattenabilityModifiers -XDenablePrimitiveClasses Point.java TestInheritedInlineTypeFields.java
  * @run main/othervm runtime.valhalla.inlinetypes.TestInheritedInlineTypeFields
  */
 
