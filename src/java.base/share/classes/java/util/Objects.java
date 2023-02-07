91c64cd16fbef1a19ae179bd071fe25d8f574948
Roger Riggs <roger.riggs@oracle.com>
2023-02-07 12:06:47 -0500

    8301899: [lworld] Value class APIs should have annotation for PreviewFeature Update tests to EnablePreview

--- src/java.base/share/classes/java/util/Objects.java
+++ src/java.base/share/classes/java/util/Objects.java
@@ -25,7 +25,7 @@
 
 package java.util;
 
-import jdk.internal.misc.ValhallaFeatures;
+import jdk.internal.javac.PreviewFeature;
 import jdk.internal.util.Preconditions;
 import jdk.internal.vm.annotation.ForceInline;
 import jdk.internal.misc.Unsafe;
@@ -197,6 +197,7 @@ public final class Objects {
     * @param obj an object
     * @throws NullPointerException if {@code obj} is {@code null}
     */
+   @PreviewFeature(feature = PreviewFeature.Feature.VALUE_OBJECTS)
 //    @IntrinsicCandidate
     public static boolean isIdentityObject(Object obj) {
         requireNonNull(obj);
@@ -214,6 +215,7 @@ public final class Objects {
      * @throws IdentityException if {@code obj} is not an identity object
      * @since Valhalla
      */
+    @PreviewFeature(feature = PreviewFeature.Feature.VALUE_OBJECTS)
     @ForceInline
     public static <T> T requireIdentity(T obj) {
         Objects.requireNonNull(obj);
@@ -234,6 +236,7 @@ public final class Objects {
      * @throws IdentityException if {@code obj} is not an identity object
      * @since Valhalla
      */
+    @PreviewFeature(feature = PreviewFeature.Feature.VALUE_OBJECTS)
     @ForceInline
     public static <T> T requireIdentity(T obj, String message) {
         Objects.requireNonNull(obj);
@@ -254,6 +257,7 @@ public final class Objects {
      * @throws IdentityException if {@code obj} is not an identity object
      * @since Valhalla
      */
+    @PreviewFeature(feature = PreviewFeature.Feature.VALUE_OBJECTS)
     @ForceInline
     public static <T> T requireIdentity(T obj, Supplier<String> messageSupplier) {
         Objects.requireNonNull(obj);
@@ -270,6 +274,7 @@ public final class Objects {
     * @param obj an object
     * @throws NullPointerException if {@code obj} is {@code null}
     */
+   @PreviewFeature(feature = PreviewFeature.Feature.VALUE_OBJECTS)
 //    @IntrinsicCandidate
     public static boolean isValueObject(Object obj) {
         requireNonNull(obj, "obj");
