0bae3585330d1c600e40274cd5bbfecff98c47a4
Tobias Hartmann <tobias.hartmann@oracle.com>
2023-02-27 07:43:38 +0100

    Fixed wrong bug number

--- src/hotspot/share/opto/castnode.cpp
+++ src/hotspot/share/opto/castnode.cpp
@@ -414,7 +414,7 @@ const Type* CheckCastPPNode::Value(PhaseGVN* phase) const {
   const TypePtr *my_type = _type->isa_ptr();
   const Type *result = _type;
   if (in_type != NULL && my_type != NULL) {
-    // TODO 8251442
+    // TODO 8302672
     if (!StressReflectiveCode && my_type->isa_aryptr() && in_type->isa_aryptr()) {
       // Propagate array properties (not flat/null-free)
       // Don't do this when StressReflectiveCode is enabled because it might lead to
