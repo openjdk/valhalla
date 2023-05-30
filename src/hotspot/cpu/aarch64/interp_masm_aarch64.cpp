a5b2b23d29af65d7f7b07086c087ba77c45b595c
David Simms <david.simms@oracle.com>
2023-05-30 10:18:18 +0200

    More adjust for ConstMethodFlags

--- src/hotspot/cpu/aarch64/interp_masm_aarch64.cpp
+++ src/hotspot/cpu/aarch64/interp_masm_aarch64.cpp
@@ -33,6 +33,7 @@
 #include "interpreter/interpreterRuntime.hpp"
 #include "logging/log.hpp"
 #include "oops/arrayOop.hpp"
+#include "oops/constMethodFlags.hpp"
 #include "oops/markWord.hpp"
 #include "oops/method.hpp"
 #include "oops/methodData.hpp"
@@ -796,7 +797,7 @@ void InterpreterMacroAssembler::remove_activation(
       Label skip_stress;
       ldr(rscratch1, Address(rfp, frame::interpreter_frame_method_offset * wordSize));
       ldrw(rscratch1, Address(rscratch1, Method::flags_offset()));
-      tstw(rscratch1, Method::scalarized_return_flag());
+      tstw(rscratch1, ConstMethodFlags::has_scalarized_return_flag());
       br(Assembler::EQ, skip_stress);
       load_klass(r0, r0);
       orr(r0, r0, 1);
