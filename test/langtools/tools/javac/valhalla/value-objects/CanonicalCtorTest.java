abd0b00a0fbbe6e0727f9665ec95f49bdbc345a0
Vicente Romero <vicente.romero@oracle.com>
2023-06-22 18:49:14 -0400

    8310679: [lw5] test refactoring, repurposing primitive classes tests

--- test/langtools/tools/javac/valhalla/value-objects/CanonicalCtorTest.java
+++ test/langtools/tools/javac/valhalla/value-objects/CanonicalCtorTest.java
@@ -24,13 +24,10 @@
 /**
  * @test
  * @bug 8208067
- * @ignore
  * @summary Verify that instance methods are callable from ctor after all instance fields are DA.
- * @compile -XDenablePrimitiveClasses CanonicalCtorTest.java
- * @run main/othervm -ea -XX:+EnableValhalla -XX:+EnablePrimitiveClasses CanonicalCtorTest
  */
 
-public primitive class CanonicalCtorTest {
+public value class CanonicalCtorTest {
 
 	private final int x, ymx;
 
