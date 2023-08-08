67a1522703f410cd9ccec52734688ca747c91a83
Vicente Romero <vicente.romero@oracle.com>
2023-08-08 15:45:07 -0400

    regression test refactoring, cleanup

--- test/langtools/tools/javac/nullability/NullabilityParsingTest.java
+++ test/langtools/tools/javac/nullability/NullabilityParsingTest.java
@@ -27,61 +27,49 @@
  * @test
  * @enablePreview
  * @summary Smoke test for parsing of bang types
- * @compile BangTypeParsingTest.java
+ * @compile NullabilityParsingTest.java
  */
 
 import java.util.function.*;
 import java.util.*;
 
-class Test {
+class NullabilityParsingTest {
     // fields
-    Object? o1;
     Object! o2;
 
     // method parameters
-    void m1(Object? o) { }
     void m2(Object! o) { }
 
     // method returns
-    Object? m1() { return null; }
-    Object! m2() { return null; }
+    Object! m2() { return new Object(); }
 
     // locals
     void testLocals() {
-        Object? o1;
         Object! o2;
     }
 
     // generics - field
-    Consumer<Object?> co1;
     Consumer<Object!> co2;
 
     // generics - method param
-    void m3(Consumer<Object?> co) { }
     void m4(Consumer<Object!> co) { }
 
     // generics - method return
-    Consumer<Object?> m3() { return null; }
     Consumer<Object!> m4() { return null; }
 
     // generics - local
     void testGenericLocals() {
-        Consumer<Object?> co1;
         Consumer<Object!> co2;
     }
 
     // lambdas
     void testLambdas() {
-        Consumer<Object?> co1 = (Object? co) -> {};
         Consumer<Object!> co2 = (Object! co) -> {};
     }
 
     void testGenericLambdas() {
-        Consumer<Consumer<Object?>> co1 = (Consumer<Object?> co) -> {};
         Consumer<Consumer<Object!>> co2 = (Consumer<Object!> co) -> {};
-        Consumer<Function<Object?, Object!>> co3 = (Function<Object?, Object!> co) -> {};
-        Consumer<Function<Object!, Object?>> co4 = (Function<Object!, Object?> co) -> {};
-        Consumer<Consumer<Consumer<Consumer<Object?>>>> co5 = (Consumer<Consumer<Consumer<Object?>>> co) -> {};
+        Consumer<Function<Object!, Object!>> co3 = (Function<Object!, Object!> co) -> {};
         Consumer<Consumer<Consumer<Consumer<Object!>>>> co6 = (Consumer<Consumer<Consumer<Object!>>> co) -> {};
     }
 
@@ -90,8 +78,8 @@ class Test {
     void testTypeTestPatterns(Object o) {
         switch (o) {
             case Integer! i -> throw new AssertionError();
-            case String? s -> throw new AssertionError();
-                default -> throw new AssertionError();
+            case String! s -> throw new AssertionError();
+            default -> throw new AssertionError();
         }
     }
 
@@ -102,9 +90,6 @@ class Test {
         switch (o) {
             case I<Integer!> i -> { }
         }
-        switch (o) {
-            case I<Integer?> i -> { }
-        }
     }
 
     sealed interface I2<X> {}
@@ -113,9 +98,6 @@ class Test {
     void genericTypeTestPatterns(A2 o) {
         switch (o) {
             case I2<I<Integer!>> i -> { }
-        }
-        switch (o) {
-            case I2<I<Integer?>> i -> { }
         }
     }
 
@@ -126,9 +108,6 @@ class Test {
         switch (o) {
             case I3<I2<I<Integer!>>> i -> { }
         }
-        switch (o) {
-            case I3<I2<I<Integer?>>> i -> { }
-        }
     }
 
     // record patterns
@@ -136,9 +115,6 @@ class Test {
     record R(A a) { }
 
     void genericRecordPatterns(R o) {
-        switch (o) {
-            case R?(I<Integer?> i) -> { }
-        }
         switch (o) {
             case R!(I<Integer!> i) -> { }
         }
@@ -148,9 +124,6 @@ class Test {
 
     void genericRecordPatterns(R2 o) {
         switch (o) {
-            case R2?(I2<I<Integer?>> i) -> { }
-        }
-        switch (o) {
             case R2!(I2<I<Integer!>> i) -> { }
         }
     }
@@ -159,9 +132,6 @@ class Test {
 
     void genericRecordPatterns(R3 o) {
         switch (o) {
-            case R3?(I3<I2<I<Integer?>>> i) -> { }
-        }
-        switch (o) {
             case R3!(I3<I2<I<Integer!>>> i) -> { }
         }
     }
@@ -169,83 +139,59 @@ class Test {
     // instanceof/cast
 
     void testInstanceOf(Object o) {
-        boolean r1 = o instanceof String?;
         boolean r2 = o instanceof String!;
     }
 
     void testInstanceRecord(R r) {
-        boolean r1 = r instanceof R(I<Integer?> i);
         boolean r2 = r instanceof R(I<Integer!> i);
     }
 
     void testCast(Object o) {
-        String? s1 = (String?)o;
         String! s2 = (String!)o;
     }
 
     void testGenericCast(A a) {
-        I<Integer?> i1 = (I<Integer?>)a;
         I<Integer!> i2 = (I<Integer!>)a;
     }
 
     void testGenericCast2(A a) {
-        I?<Integer?> i1 = (I?<Integer?>)a;
         I!<Integer!> i2 = (I!<Integer!>)a;
     }
 
     // arrays
 
-    Object?[]![]?[]! oarr;
-    Function?<Object?[]![]?, Function<Object?[]![]?, Object?[]![]?>>[]![]? garr;
-
-    // patterns and for-each
-
-    void forEachPatterns(List<R> o) {
-        for (R?(I<Integer?> i) : o) { }
-        for (R!(I<Integer!> i) : o) { }
-    }
-
-    void forEachPatterns2(List<R2> o) {
-        for (R2?(I2<I?<Integer?>> i) : o) { }
-        for (R2 !(I2<I!<Integer!>> i) : o) { }
-    }
-
-    void forEachPatterns3(List<R3> o) {
-        for (R3?(I3?<I2?<I<Integer?>>> i) : o) { }
-        for (R3!(I3!<I2!<I<Integer!>>> i) : o) { }
-    }
+    Object![]![]![]! oarr;
+    Function!<Object![]![]!, Function<Object![]![]!, Object![]![]!>>[]![]! garr;
 
     void mBad1(Object o) {
         String s1 = o instanceof String ? (String)o : null;
-        String s2 = o instanceof String? ? (String)o : null;
+        String s2 = o instanceof String! ? (String)o : null;
     }
 
     void mBad2(Object o) {
         String s1 = o instanceof String ? "" : null;
-        String s2 = o instanceof String? ? "" : null;
+        String s2 = o instanceof String! ? "" : null;
     }
 
     void testPatternRule(Object o) {
         switch (o) {
-            case String? s -> { }
+            case String! s -> { }
                 default -> { }
         }
     }
 
     void testPatternCol(Object o) {
         switch (o) {
-            case String? s: { }
+            case String! s: { }
             default: { }
         }
     }
 
     void testInstanceOfAndInfix1(Object a, boolean b) {
-        boolean x1 = a instanceof String? && b;
         boolean x2 = a instanceof String! && b;
     }
 
     void testInstanceOfAndInfix2(Object a, boolean b) {
-        boolean x1 = a instanceof String? s && b;
         boolean x2 = a instanceof String! s && b;
     }
 }
