fd127e1f9be1a7457aac0034879445c4e146d786
Jatin Bhateja <jatin.bhateja@intel.com>
2024-03-19 10:42:30 +0530

    Refining for macrofusion.

--- src/hotspot/cpu/x86/assembler_x86.hpp
+++ src/hotspot/cpu/x86/assembler_x86.hpp
@@ -1730,8 +1730,6 @@ private:
   void btsq(Address dst, int imm8);
   void btrq(Address dst, int imm8);
 #endif
-  void btl(Register dst, int imm8);
-
   void orw(Register dst, Register src);
 
   void orl(Address dst, int32_t imm32);
