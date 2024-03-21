50fcd434d0fde1b67f6072ba390fec0e64ed6fa1
Tobias Hartmann <thartmann@openjdk.org>
2024-03-21 13:17:12 +0000

    8328682: [lworld] Missing barrier after value object construction

--- test/jdk/java/lang/Class/GenericStringTest.java
+++ test/jdk/java/lang/Class/GenericStringTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013, 2016, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2013, 2024, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -26,6 +26,7 @@
  * @bug 6298888 6992705 8161500 6304578
  * @summary Check Class.toGenericString()
  * @author Joseph D. Darcy
+ * @enablePreview
  * @compile GenericStringTest.java
  * @run main/othervm -XX:+EnableValhalla GenericStringTest
  */
