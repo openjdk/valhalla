6a32bf994b8cdf67957cbbcb1157e5ee7646d834
Vicente Romero <vicente.romero@oracle.com>
2024-05-08 20:54:57 -0400

    minor refactorings

--- src/jdk.compiler/share/classes/com/sun/tools/javac/code/Symtab.java
+++ src/jdk.compiler/share/classes/com/sun/tools/javac/code/Symtab.java
@@ -29,10 +29,8 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.EnumSet;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.LinkedHashMap;
 import java.util.Map;
-import java.util.Set;
 
 import javax.lang.model.element.ElementVisitor;
 
