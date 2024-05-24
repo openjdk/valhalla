64a81d2aa07c30c671e6f5b79ad14cb3c960953e
David Holmes <david.holmes@oracle.com>
2024-05-24 00:04:42 -0400

    Expand tests and ensure value classes are defined with required class modifiers

--- test/hotspot/jtreg/runtime/valhalla/inlinetypes/verifier/StrictFields.java
+++ test/hotspot/jtreg/runtime/valhalla/inlinetypes/verifier/StrictFields.java
@@ -30,8 +30,12 @@
 
 public class StrictFields {
     public static void main(String[] args) throws Throwable {
-        // First load a good class as a sanity check
-        Class<?> c = Class.forName("StrictBase");
+
+        // First load a class where strict should be ignored
+        Class<?> c = Class.forName("StrictIgnore");
+
+        // Now load a well-formed strict-using value class
+        c = Class.forName("StrictBase");
 
         // Now a bad class
         try {
@@ -44,5 +48,37 @@ public class StrictFields {
                 System.out.println("Expected VerifyError was thrown");
             }
         }
+
+        // Now a bad class that tries to write to a super class's strict field
+        // in the preinit phase
+        try {
+            c = Class.forName("BadStrictSubPreInit");
+            throw new Error("VerifyError was not thrown as expected!");
+        } catch (VerifyError ve) {
+            if (!ve.getMessage().startsWith("Bad type on operand stack")) {
+                throw new Error("Wrong VerifyError thrown", ve);
+            } else {
+                System.out.println("Expected VerifyError was thrown");
+            }
+        }
+
+        // Now a bad class that tries to write to a super class's strict field
+        // in the post phase. This is not a verification error but we test it
+        // here for completeness.Expected exception:
+        //    java.lang.IllegalAccessError: Update to non-static final field
+        //      BadStrictSubPostInit.x attempted from a different class
+        //       (BadStrictSubPostInit) than the field's declaring class
+        try {
+            c = Class.forName("BadStrictSubPostInit");
+            Object o = c.newInstance();
+            throw new Error("IllegalAccessErrorError was not thrown as expected!");
+        } catch (IllegalAccessError iae) {
+            if (!iae.getMessage().startsWith("Update to non-static final field")) {
+                throw new Error("Wrong IllegalAccessError thrown", iae);
+            } else {
+                System.out.println("Expected IllegalAccessError was thrown");
+            }
+        }
+
     }
 }
