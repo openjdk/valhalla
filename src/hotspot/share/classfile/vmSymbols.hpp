0a87e69cddf34dbe1adcec873e83216684c18c2e
Frederic Parain <fparain@openjdk.org>
2024-06-11 18:02:16 +0000

    8334034: [lworld] The JVM must recognize the new format of the Preaload/LoadableDescriptors attribute

--- src/hotspot/share/classfile/vmSymbols.hpp
+++ src/hotspot/share/classfile/vmSymbols.hpp
@@ -184,7 +184,7 @@ class SerializeClosure;
   template(tag_inner_classes,                         "InnerClasses")                             \
   template(tag_nest_members,                          "NestMembers")                              \
   template(tag_nest_host,                             "NestHost")                                 \
-  template(tag_preload,                               "Preload")                                  \
+  template(tag_loadable_descriptors,                  "LoadableDescriptors")                      \
   template(tag_constant_value,                        "ConstantValue")                            \
   template(tag_code,                                  "Code")                                     \
   template(tag_exceptions,                            "Exceptions")                               \
