450c9a4d5d40922024f4ab479096b4396ec54b35
Roger Riggs <roger.riggs@oracle.com>
2024-10-15 17:01:17 -0400

    8342276: [lworld] Apply LIBRARY.properties to stabiliize --enable-preview for tests Create LIBRARY.properties files to set the value of `enablePreview` for sensitive test library roots.
    The top level test library is set with `enablePreview = false` so that
    when compiled they will be able to be used with or without enablePreview.
    
    Individual tests that use preview features and test library files that use
    preview features will need to be compiled based on the settings in each test.
    
    Some tests are modified to remove explicit building or compilation and
    fall back to implicit compiles with the prevaining enablePreview setting.

--- test/langtools/tools/javac/completionDeps/DepsAndAnno.java
+++ test/langtools/tools/javac/completionDeps/DepsAndAnno.java
@@ -25,6 +25,7 @@
  * @test
  * @bug 8078600
  * @summary Make sure -XDcompletionDeps does not cause an infinite loop.
+ * @nablePreview
  * @library /tools/lib
  * @modules jdk.compiler/com.sun.tools.javac.api
  *          jdk.compiler/com.sun.tools.javac.main
