b258586bc2c3e6acc2bcb95abdc37b5874979fcf
Vicente Romero <vicente.romero@oracle.com>
2025-02-07 15:48:20 -0500

    refactoring n

--- src/jdk.compiler/share/classes/com/sun/tools/javac/jvm/Gen.java
+++ src/jdk.compiler/share/classes/com/sun/tools/javac/jvm/Gen.java
@@ -996,7 +996,7 @@ public class Gen extends JCTree.Visitor {
                 int startpcCrt = initCode(tree, env, fatcode);
                 Set<VarSymbol> prevUnsetFields = code.currentUnsetFields;
                 if (meth.isConstructor()) {
-                    code.currentUnsetFields = unsetFieldsInfo.getUnsetFields(env.enclClass.sym, tree.body);;
+                    code.currentUnsetFields = unsetFieldsInfo.getUnsetFields(env.enclClass.sym, tree.body);
                 }
 
                 try {
@@ -1624,106 +1624,110 @@ public class Gen extends JCTree.Visitor {
         void genTry(JCTree body, List<JCCatch> catchers, Env<GenContext> env) {
             Set<VarSymbol> prevCodeUnsetFields = code.currentUnsetFields;
             try {
-                int limit = code.nextreg;
-                int startpc = code.curCP();
-                Code.State stateTry = code.state.dup();
-                genStat(body, env, CRT_BLOCK);
-                int endpc = code.curCP();
-                List<Integer> gaps = env.info.gaps.toList();
-                code.statBegin(TreeInfo.endPos(body));
-                genFinalizer(env);
-                code.statBegin(TreeInfo.endPos(env.tree));
-                Chain exitChain;
-                boolean actualTry = env.tree.hasTag(TRY);
-                if (startpc == endpc && actualTry) {
-                    exitChain = code.branch(dontgoto);
-                } else {
-                    exitChain = code.branch(goto_);
-                }
-                endFinalizerGap(env);
-                env.info.finalize.afterBody();
-                boolean hasFinalizer =
-                        env.info.finalize != null &&
-                                env.info.finalize.hasFinalizer();
-                if (startpc != endpc) for (List<JCCatch> l = catchers; l.nonEmpty(); l = l.tail) {
-                    // start off with exception on stack
-                    code.entryPoint(stateTry, l.head.param.sym.type);
-                    genCatch(l.head, env, startpc, endpc, gaps);
-                    genFinalizer(env);
-                    if (hasFinalizer || l.tail.nonEmpty()) {
-                        code.statBegin(TreeInfo.endPos(env.tree));
-                        exitChain = Code.mergeChains(exitChain,
-                                code.branch(goto_));
-                    }
-                    endFinalizerGap(env);
-                }
-                if (hasFinalizer && (startpc != endpc || !actualTry)) {
-                    // Create a new register segment to avoid allocating
-                    // the same variables in finalizers and other statements.
-                    code.newRegSegment();
-
-                    // Add a catch-all clause.
-
-                    // start off with exception on stack
-                    int catchallpc = code.entryPoint(stateTry, syms.throwableType);
-
-                    // Register all exception ranges for catch all clause.
-                    // The range of the catch all clause is from the beginning
-                    // of the try or synchronized block until the present
-                    // code pointer excluding all gaps in the current
-                    // environment's GenContext.
-                    int startseg = startpc;
-                    while (env.info.gaps.nonEmpty()) {
-                        int endseg = env.info.gaps.next().intValue();
-                        registerCatch(body.pos(), startseg, endseg,
-                                catchallpc, 0);
-                        startseg = env.info.gaps.next().intValue();
-                    }
-                    code.statBegin(TreeInfo.finalizerPos(env.tree, PosKind.FIRST_STAT_POS));
-                    code.markStatBegin();
-
-                    Item excVar = makeTemp(syms.throwableType);
-                    excVar.store();
-                    genFinalizer(env);
-                    code.resolvePending();
-                    code.statBegin(TreeInfo.finalizerPos(env.tree, PosKind.END_POS));
-                    code.markStatBegin();
-
-                    excVar.load();
-                    registerCatch(body.pos(), startseg,
-                            env.info.gaps.next().intValue(),
-                            catchallpc, 0);
-                    code.emitop0(athrow);
-                    code.markDead();
-
-                    // If there are jsr's to this finalizer, ...
-                    if (env.info.cont != null) {
-                        // Resolve all jsr's.
-                        code.resolve(env.info.cont);
-
-                        // Mark statement line number
-                        code.statBegin(TreeInfo.finalizerPos(env.tree, PosKind.FIRST_STAT_POS));
-                        code.markStatBegin();
-
-                        // Save return address.
-                        LocalItem retVar = makeTemp(syms.throwableType);
-                        retVar.store();
-
-                        // Generate finalizer code.
-                        env.info.finalize.genLast();
-
-                        // Return.
-                        code.emitop1w(ret, retVar.reg);
-                        code.markDead();
-                    }
-                }
-                // Resolve all breaks.
-                code.resolve(exitChain);
-
-                code.endScopes(limit);
+                genTryHelper(body, catchers, env);
             } finally {
                 code.currentUnsetFields = prevCodeUnsetFields;
             }
+        }
+
+        void genTryHelper(JCTree body, List<JCCatch> catchers, Env<GenContext> env) {
+            int limit = code.nextreg;
+            int startpc = code.curCP();
+            Code.State stateTry = code.state.dup();
+            genStat(body, env, CRT_BLOCK);
+            int endpc = code.curCP();
+            List<Integer> gaps = env.info.gaps.toList();
+            code.statBegin(TreeInfo.endPos(body));
+            genFinalizer(env);
+            code.statBegin(TreeInfo.endPos(env.tree));
+            Chain exitChain;
+            boolean actualTry = env.tree.hasTag(TRY);
+            if (startpc == endpc && actualTry) {
+                exitChain = code.branch(dontgoto);
+            } else {
+                exitChain = code.branch(goto_);
+            }
+            endFinalizerGap(env);
+            env.info.finalize.afterBody();
+            boolean hasFinalizer =
+                    env.info.finalize != null &&
+                            env.info.finalize.hasFinalizer();
+            if (startpc != endpc) for (List<JCCatch> l = catchers; l.nonEmpty(); l = l.tail) {
+                // start off with exception on stack
+                code.entryPoint(stateTry, l.head.param.sym.type);
+                genCatch(l.head, env, startpc, endpc, gaps);
+                genFinalizer(env);
+                if (hasFinalizer || l.tail.nonEmpty()) {
+                    code.statBegin(TreeInfo.endPos(env.tree));
+                    exitChain = Code.mergeChains(exitChain,
+                            code.branch(goto_));
+                }
+                endFinalizerGap(env);
+            }
+            if (hasFinalizer && (startpc != endpc || !actualTry)) {
+                // Create a new register segment to avoid allocating
+                // the same variables in finalizers and other statements.
+                code.newRegSegment();
+
+                // Add a catch-all clause.
+
+                // start off with exception on stack
+                int catchallpc = code.entryPoint(stateTry, syms.throwableType);
+
+                // Register all exception ranges for catch all clause.
+                // The range of the catch all clause is from the beginning
+                // of the try or synchronized block until the present
+                // code pointer excluding all gaps in the current
+                // environment's GenContext.
+                int startseg = startpc;
+                while (env.info.gaps.nonEmpty()) {
+                    int endseg = env.info.gaps.next().intValue();
+                    registerCatch(body.pos(), startseg, endseg,
+                            catchallpc, 0);
+                    startseg = env.info.gaps.next().intValue();
+                }
+                code.statBegin(TreeInfo.finalizerPos(env.tree, PosKind.FIRST_STAT_POS));
+                code.markStatBegin();
+
+                Item excVar = makeTemp(syms.throwableType);
+                excVar.store();
+                genFinalizer(env);
+                code.resolvePending();
+                code.statBegin(TreeInfo.finalizerPos(env.tree, PosKind.END_POS));
+                code.markStatBegin();
+
+                excVar.load();
+                registerCatch(body.pos(), startseg,
+                        env.info.gaps.next().intValue(),
+                        catchallpc, 0);
+                code.emitop0(athrow);
+                code.markDead();
+
+                // If there are jsr's to this finalizer, ...
+                if (env.info.cont != null) {
+                    // Resolve all jsr's.
+                    code.resolve(env.info.cont);
+
+                    // Mark statement line number
+                    code.statBegin(TreeInfo.finalizerPos(env.tree, PosKind.FIRST_STAT_POS));
+                    code.markStatBegin();
+
+                    // Save return address.
+                    LocalItem retVar = makeTemp(syms.throwableType);
+                    retVar.store();
+
+                    // Generate finalizer code.
+                    env.info.finalize.genLast();
+
+                    // Return.
+                    code.emitop1w(ret, retVar.reg);
+                    code.markDead();
+                }
+            }
+            // Resolve all breaks.
+            code.resolve(exitChain);
+
+            code.endScopes(limit);
         }
 
         /** Generate code for a catch clause.
