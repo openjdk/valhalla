e21072874a4cf965a4b28c5826848d1e8d14e874
Quan Anh Mai <anhmdq99@gmail.com>
2025-03-03 01:24:38 +0700

    assert non-larval

--- src/hotspot/share/opto/memnode.cpp
+++ src/hotspot/share/opto/memnode.cpp
@@ -1100,7 +1100,9 @@ Node* LoadNode::can_see_arraycopy_value(Node* st, PhaseGVN* phase) const {
 
 static Node* see_through_inline_type(PhaseValues* phase, const MemNode* load, Node* base, int offset) {
   if (!load->is_mismatched_access() && base != nullptr && base->is_InlineType() && offset > oopDesc::klass_offset_in_bytes()) {
-    Node* value = base->as_InlineType()->field_value_by_offset(offset, true);
+    InlineTypeNode* vt = base->as_InlineType();
+    assert(!vt->is_larval(), "must not load from a larval object");
+    Node* value = vt->field_value_by_offset(offset, true);
     assert(value != nullptr, "must see some value");
     return value;
   }
