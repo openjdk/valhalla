dbe405f217766565c72889644bb8021a96b558ea
Tobias Hartmann <tobias.hartmann@oracle.com>
2025-04-25 12:39:11 +0200

    8354322: [lworld] InlineKlass::collect_fields fails to look up flat fields declared in abstract super class

--- test/hotspot/jtreg/compiler/valhalla/inlinetypes/MyAbstract.java
+++ test/hotspot/jtreg/compiler/valhalla/inlinetypes/MyAbstract.java
@@ -24,6 +24,8 @@
 package compiler.valhalla.inlinetypes;
 
 import jdk.internal.vm.annotation.LooselyConsistentValue;
+import jdk.internal.vm.annotation.NullRestricted;
+import jdk.internal.vm.annotation.Strict;
 
 @LooselyConsistentValue
 public abstract value class MyAbstract implements MyInterface {
