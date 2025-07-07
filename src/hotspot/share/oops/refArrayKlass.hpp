8e80376011dc85cf5660b5c497216ce823ee38fd
Frederic Parain <frederic.parain@oracle.com>
2025-07-07 09:26:57 -0400

    Fix indentation

--- src/hotspot/share/oops/refArrayKlass.hpp
+++ src/hotspot/share/oops/refArrayKlass.hpp
@@ -43,96 +43,97 @@ class RefArrayKlass : public ObjArrayKlass {
  private:
   // Constructor
   RefArrayKlass(int n, Klass *element_klass, Symbol *name, ArrayKlass::ArrayProperties props);
-         static RefArrayKlass* allocate_klass(ClassLoaderData *loader_data, int n, Klass *k, Symbol *name, ArrayKlass::ArrayProperties props, TRAPS);
+  static RefArrayKlass* allocate_klass(ClassLoaderData *loader_data, int n, Klass *k, Symbol *name,
+				       ArrayKlass::ArrayProperties props, TRAPS);
 
  public:
-   // For dummy objects
-         RefArrayKlass() {}
+  // For dummy objects
+  RefArrayKlass() {}
 
-         // Dispatched operation
-         DEBUG_ONLY(bool is_refArray_klass_slow() const { return true; })
-         size_t oop_size(oop obj) const;  // TODO FIXME make it virtual in objArrayKlass
+  // Dispatched operation
+  DEBUG_ONLY(bool is_refArray_klass_slow() const { return true; })
+  size_t oop_size(oop obj) const;  // TODO FIXME make it virtual in objArrayKlass
 
-         // Allocation
-         static RefArrayKlass *allocate_refArray_klass(ClassLoaderData *loader_data,
-                                                                                                        int n, Klass *element_klass,
-                                                                                                        ArrayKlass::ArrayProperties props, TRAPS);
+  // Allocation
+  static RefArrayKlass *allocate_refArray_klass(ClassLoaderData *loader_data,
+						int n, Klass *element_klass,
+						ArrayKlass::ArrayProperties props, TRAPS);
 
-         objArrayOop allocate_instance(int length, ArrayProperties props, TRAPS);
+  objArrayOop allocate_instance(int length, ArrayProperties props, TRAPS);
 
-         // Copying TODO FIXME make copying method in objArrayKlass virtual and default implementation invalid (ShouldNotReachHere())
-         void copy_array(arrayOop s, int src_pos, arrayOop d, int dst_pos, int length, TRAPS);
+  // Copying TODO FIXME make copying method in objArrayKlass virtual and default implementation invalid (ShouldNotReachHere())
+  void copy_array(arrayOop s, int src_pos, arrayOop d, int dst_pos, int length, TRAPS);
 
-         virtual void metaspace_pointers_do(MetaspaceClosure *iter);
+  virtual void metaspace_pointers_do(MetaspaceClosure *iter);
 
  private:
-         // Either oop or narrowOop depending on UseCompressedOops.
-         // must be called from within ObjArrayKlass.cpp
-         void do_copy(arrayOop s, size_t src_offset,
-                                                                arrayOop d, size_t dst_offset,
-                                                                int length, TRAPS);
+  // Either oop or narrowOop depending on UseCompressedOops.
+  // must be called from within ObjArrayKlass.cpp
+  void do_copy(arrayOop s, size_t src_offset,
+	       arrayOop d, size_t dst_offset,
+	       int length, TRAPS);
 
  public:
-         static RefArrayKlass *cast(Klass *k) {
-                 return const_cast<RefArrayKlass *>(cast(const_cast<const Klass *>(k)));
-         }
+  static RefArrayKlass *cast(Klass *k) {
+    return const_cast<RefArrayKlass *>(cast(const_cast<const Klass *>(k)));
+  }
 
-         static const RefArrayKlass *cast(const Klass *k) {
-                 assert(k->is_refArray_klass(), "cast to RefArrayKlass");
-                 return static_cast<const RefArrayKlass *>(k);
-         }
+  static const RefArrayKlass *cast(const Klass *k) {
+    assert(k->is_refArray_klass(), "cast to RefArrayKlass");
+    return static_cast<const RefArrayKlass *>(k);
+  }
 
-         // Sizing
-         static int header_size() { return sizeof(RefArrayKlass) / wordSize; }
-         int size() const { return ArrayKlass::static_size(header_size()); }
+  // Sizing
+  static int header_size() { return sizeof(RefArrayKlass) / wordSize; }
+  int size() const { return ArrayKlass::static_size(header_size()); }
 
-         // Initialization (virtual from Klass)
-         void initialize(TRAPS);
+  // Initialization (virtual from Klass)
+  void initialize(TRAPS);
 
-         // Oop fields (and metadata) iterators
-         //
-         // The RefArrayKlass iterators also visits the Object's klass.
+  // Oop fields (and metadata) iterators
+  //
+  // The RefArrayKlass iterators also visits the Object's klass.
 
-         // Iterate over oop elements and metadata.
-         template <typename T, typename OopClosureType>
-         inline void oop_oop_iterate(oop obj, OopClosureType *closure);
+  // Iterate over oop elements and metadata.
+  template <typename T, typename OopClosureType>
+  inline void oop_oop_iterate(oop obj, OopClosureType *closure);
 
-         // Iterate over oop elements and metadata.
-         template <typename T, typename OopClosureType>
-         inline void oop_oop_iterate_reverse(oop obj, OopClosureType *closure);
+  // Iterate over oop elements and metadata.
+  template <typename T, typename OopClosureType>
+  inline void oop_oop_iterate_reverse(oop obj, OopClosureType *closure);
 
-         // Iterate over oop elements within mr, and metadata.
-         template <typename T, typename OopClosureType>
-         inline void oop_oop_iterate_bounded(oop obj, OopClosureType *closure, MemRegion mr);
+  // Iterate over oop elements within mr, and metadata.
+  template <typename T, typename OopClosureType>
+  inline void oop_oop_iterate_bounded(oop obj, OopClosureType *closure, MemRegion mr);
 
-         // Iterate over oop elements within [start, end), and metadata.
-         template <typename T, class OopClosureType>
-         inline void oop_oop_iterate_range(refArrayOop a, OopClosureType *closure, int start, int end);
+  // Iterate over oop elements within [start, end), and metadata.
+  template <typename T, class OopClosureType>
+  inline void oop_oop_iterate_range(refArrayOop a, OopClosureType *closure, int start, int end);
 
  public:
-         // Iterate over all oop elements.
-         template <typename T, class OopClosureType>
-         inline void oop_oop_iterate_elements(refArrayOop a, OopClosureType *closure);
+  // Iterate over all oop elements.
+  template <typename T, class OopClosureType>
+  inline void oop_oop_iterate_elements(refArrayOop a, OopClosureType *closure);
 
  private:
-         // Iterate over all oop elements with indices within mr.
-         template <typename T, class OopClosureType>
-         inline void oop_oop_iterate_elements_bounded(refArrayOop a, OopClosureType *closure, void *low, void *high);
+  // Iterate over all oop elements with indices within mr.
+  template <typename T, class OopClosureType>
+  inline void oop_oop_iterate_elements_bounded(refArrayOop a, OopClosureType *closure, void *low, void *high);
 
  public:
-         // Printing
-         void print_on(outputStream *st) const;
-         void print_value_on(outputStream *st) const;
+  // Printing
+  void print_on(outputStream *st) const;
+  void print_value_on(outputStream *st) const;
 
-         void oop_print_value_on(oop obj, outputStream *st);
+  void oop_print_value_on(oop obj, outputStream *st);
 #ifndef PRODUCT
-         void oop_print_on(oop obj, outputStream *st);
+  void oop_print_on(oop obj, outputStream *st);
 #endif // PRODUCT
 
-         // Verification
-         void verify_on(outputStream *st);
+  // Verification
+  void verify_on(outputStream *st);
 
-         void oop_verify_on(oop obj, outputStream *st);
+  void oop_verify_on(oop obj, outputStream *st);
 };
 
 #endif // SHARE_OOPS_REFARRAYKLASS_HPP
