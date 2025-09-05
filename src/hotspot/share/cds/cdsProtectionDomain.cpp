b27def5761f9e1e35de9a0be4b080931cbaebbd6
Frederic Parain <frederic.parain@oracle.com>
2025-09-05 07:59:54 -0400

    Remove commented code

--- src/hotspot/share/cds/cdsProtectionDomain.cpp
+++ src/hotspot/share/cds/cdsProtectionDomain.cpp
@@ -297,7 +297,6 @@ void CDSProtectionDomain::atomic_set_array_index(OopHandle array, int index, oop
   // The important thing here is that all threads pick up the same result.
   // It doesn't matter which racing thread wins, as long as only one
   // result is used by all threads, and all future queries.
-  // ((objArrayOop)array.resolve())->replace_if_null(index, o);
   refArrayOopDesc::cast(array.resolve())->replace_if_null(index, o);
 }
 
