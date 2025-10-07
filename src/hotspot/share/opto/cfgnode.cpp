619825cd7640af99253455dda68e8b9d9018d478
Christian Hagedorn <chagedorn@openjdk.org>
2025-10-07 07:24:00 +0000

    8368782: [lworld] C2: fatal error: no reachable node should have no use
    Reviewed-by: thartmann

--- src/hotspot/share/opto/cfgnode.cpp
+++ src/hotspot/share/opto/cfgnode.cpp
@@ -2694,7 +2694,7 @@ Node *PhiNode::Ideal(PhaseGVN *phase, bool can_reshape) {
       Node *ii = in(i);
       Node *new_in = MemNode::optimize_memory_chain(ii, at, nullptr, phase);
       if (ii != new_in ) {
-        set_req(i, new_in);
+        set_req_X(i, new_in, phase->is_IterGVN());
         progress = this;
       }
     }
