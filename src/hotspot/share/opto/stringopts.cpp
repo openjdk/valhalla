140d9e3582c579751d74ed921ec798c2bb141e77
Damon Fenacci <damon.fenacci@oracle.com>
2025-10-17 11:13:05 +0200

    JDK-8367405: ad-hoc CheckCastPP handling

--- src/hotspot/share/opto/stringopts.cpp
+++ src/hotspot/share/opto/stringopts.cpp
@@ -1135,13 +1135,18 @@ bool StringConcat::validate_control_flow() {
         ctrl_path.push(use);
         continue;
       }
-      if (opc == Op_CastPP || opc == Op_CheckCastPP) {
+      if (opc == Op_CheckCastPP) {
         worklist.push(use);
         worklist.push(use->in(1));
         ctrl_path.push(use);
         continue;
       }
-      if (opc == Op_Phi && skip_string_null_check(use) != use) {
+      if (opc == Op_CastPP) {
+        for (SimpleDUIterator j(use); j.has_next(); j.next()) {
+          worklist.push(j.get());
+        }
+        worklist.push(use->in(1));
+        ctrl_path.push(use);
         continue;
       }
 #ifndef PRODUCT
--- test/hotspot/jtreg/compiler/c2/Test7179138_1.java
+++ test/hotspot/jtreg/compiler/c2/Test7179138_1.java
@@ -1,6 +1,5 @@
 /*
  * Copyright 2012, 2021, Skip Balk.  All Rights Reserved.
- * Copyright (c) 2025, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -25,69 +24,50 @@
 /*
  * @test
  * @key stress randomness
- * @bug 7179138 8271341 8367405
+ * @bug 7179138 8271341
  * @summary Incorrect result with String concatenation optimization
- * @library /test/lib /
- * @run driver compiler.c2.Test7179138_1
+ *
+ * @run main/othervm -Xbatch -XX:+IgnoreUnrecognizedVMOptions -XX:-TieredCompilation
+ *      compiler.c2.Test7179138_1
+ * @run main/othervm -Xbatch -XX:+IgnoreUnrecognizedVMOptions -XX:-TieredCompilation
+ *      -XX:+UnlockDiagnosticVMOptions -XX:+StressIGVN compiler.c2.Test7179138_1
+ * @run main/othervm -Xbatch -XX:+IgnoreUnrecognizedVMOptions -XX:-TieredCompilation
+ *      -XX:+UnlockDiagnosticVMOptions -XX:+StressIGVN -XX:+AlwaysIncrementalInline
+ *      compiler.c2.Test7179138_1
  *
  * @author Skip Balk
  */
 
 package compiler.c2;
 
-import jdk.test.lib.process.ProcessTools;
-import jdk.test.lib.process.OutputAnalyzer;
-
 public class Test7179138_1 {
     public static void main(String[] args) throws Exception {
-        if (args.length == 0) {
-            ProcessBuilder pb = ProcessTools.createLimitedTestJavaProcessBuilder("-Xbatch", "-XX:+IgnoreUnrecognizedVMOptions",
-                "-XX:-TieredCompilation", "-XX:+PrintOptimizeStringConcat", "compiler.c2.Test7179138_1", "run");
-            OutputAnalyzer output = new OutputAnalyzer(pb.start());
-            output.shouldHaveExitValue(0);
-            output.shouldNotContain("stacking would fail");
+        System.out.println("Java Version: " + System.getProperty("java.vm.version"));
+        long[] durations = new long[60];
+        for (int i = 0; i < 100000; i++) {
+            // this empty for-loop is required to reproduce this bug
+            for (long duration : durations) {
+                // do nothing
+            }
+            {
+                String s = "test";
+                int len = s.length();
 
-            pb = ProcessTools.createLimitedTestJavaProcessBuilder("-Xbatch", "-XX:+IgnoreUnrecognizedVMOptions",
-                "-XX:-TieredCompilation", "-XX:+PrintOptimizeStringConcat", "-XX:+UnlockDiagnosticVMOptions", "-XX:+StressIGVN",
-                "compiler.c2.Test7179138_1", "run");
-            output = new OutputAnalyzer(pb.start());
-            output.shouldHaveExitValue(0);
-            output.shouldNotContain("stacking would fail");
+                s = new StringBuilder(String.valueOf(s)).append(s).toString();
+                len = len + len;
 
-            pb = ProcessTools.createLimitedTestJavaProcessBuilder("-Xbatch", "-XX:+IgnoreUnrecognizedVMOptions",
-                "-XX:-TieredCompilation", "-XX:+PrintOptimizeStringConcat", "-XX:+UnlockDiagnosticVMOptions", "-XX:+StressIGVN",
-                "-XX:+AlwaysIncrementalInline", "compiler.c2.Test7179138_1", "run");
-            output = new OutputAnalyzer(pb.start());
-            output.shouldHaveExitValue(0);
-            output.shouldNotContain("stacking would fail");
-        } else {
-            System.out.println("Java Version: " + System.getProperty("java.vm.version"));
-            long[] durations = new long[60];
-            for (int i = 0; i < 100000; i++) {
-                // this empty for-loop is required to reproduce this bug
-                for (long duration : durations) {
-                    // do nothing
-                }
-                {
-                    String s = "test";
-                    int len = s.length();
+                s = new StringBuilder(String.valueOf(s)).append(s).toString();
+                len = len + len;
 
-                    s = new StringBuilder(String.valueOf(s)).append(s).toString();
-                    len = len + len;
+                s = new StringBuilder(String.valueOf(s)).append(s).toString();
+                len = len + len;
 
-                    s = new StringBuilder(String.valueOf(s)).append(s).toString();
-                    len = len + len;
-
-                    s = new StringBuilder(String.valueOf(s)).append(s).toString();
-                    len = len + len;
-
-                    if (s.length() != len) {
-                        System.out.println("Failed at iteration: " + i);
-                        System.out.println("Length mismatch: " + s.length() + " <> " + len);
-                        System.out.println("Expected: \"" + "test" + "test" + "test" + "test" + "test" + "test" + "test" + "test" + "\"");
-                        System.out.println("Actual:   \"" + s + "\"");
-                        System.exit(97);
-                    }
+                if (s.length() != len) {
+                    System.out.println("Failed at iteration: " + i);
+                    System.out.println("Length mismatch: " + s.length() + " <> " + len);
+                    System.out.println("Expected: \"" + "test" + "test" + "test" + "test" + "test" + "test" + "test" + "test" + "\"");
+                    System.out.println("Actual:   \"" + s + "\"");
+                    System.exit(97);
                 }
             }
         }
