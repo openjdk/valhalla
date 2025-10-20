a75ca117bc4e686d863b468158bd58eba424fcb6
Paul Hübner <paul.hubner@oracle.com>
2025-10-20 10:00:37 +0200

    WhiteBox API.

--- test/hotspot/jtreg/runtime/valhalla/inlinetypes/LarvalMarkWordTest.java
+++ test/hotspot/jtreg/runtime/valhalla/inlinetypes/LarvalMarkWordTest.java
@@ -24,42 +24,71 @@
 /**
  * @test
  * @summary ensure larval bit is kept by Serial
+ * @library /test/lib /
  * @requires vm.flagless
  * @enablePreview
  * @modules java.base/jdk.internal.misc
- * @run main/othervm -XX:+UseSerialGC -Xmx50M -Xlog:gc*=info runtime.valhalla.inlinetypes.LarvalMarkWordTest
+            java.management
+ * @build jdk.test.whitebox.WhiteBox
+ * @run driver jdk.test.lib.helpers.ClassFileInstaller jdk.test.whitebox.WhiteBox
+ * @run main/othervm -XX:+UseSerialGC -Xlog:gc*=info
+                     -Xbootclasspath/a:. -XX:+UnlockDiagnosticVMOptions -XX:+WhiteBoxAPI
+                     runtime.valhalla.inlinetypes.LarvalMarkWordTest
  */
+
 /**
  * @test
  * @summary ensure larval bit is kept by Parallel
+ * @library /test/lib /
  * @requires vm.flagless
  * @enablePreview
  * @modules java.base/jdk.internal.misc
- * @run main/othervm -XX:+UseParallelGC -Xmx50M -Xlog:gc*=info runtime.valhalla.inlinetypes.LarvalMarkWordTest
+            java.management
+ * @build jdk.test.whitebox.WhiteBox
+ * @run driver jdk.test.lib.helpers.ClassFileInstaller jdk.test.whitebox.WhiteBox
+ * @run main/othervm -XX:+UseParallelGC -Xlog:gc*=info
+                     -Xbootclasspath/a:. -XX:+UnlockDiagnosticVMOptions -XX:+WhiteBoxAPI
+                     runtime.valhalla.inlinetypes.LarvalMarkWordTest
  */
+
 /**
  * @test
  * @summary ensure larval bit is kept by G1
+ * @library /test/lib /
  * @requires vm.flagless
  * @enablePreview
  * @modules java.base/jdk.internal.misc
- * @run main/othervm -XX:+UseG1GC -Xmx50M -Xlog:gc*=info runtime.valhalla.inlinetypes.LarvalMarkWordTest
+            java.management
+ * @build jdk.test.whitebox.WhiteBox
+ * @run driver jdk.test.lib.helpers.ClassFileInstaller jdk.test.whitebox.WhiteBox
+ * @run main/othervm -XX:+UseG1GC -Xlog:gc*=info
+                     -Xbootclasspath/a:. -XX:+UnlockDiagnosticVMOptions -XX:+WhiteBoxAPI
+                     runtime.valhalla.inlinetypes.LarvalMarkWordTest
  */
+
 /**
  * @test
  * @summary ensure larval bit is kept by Z
+ * @library /test/lib /
  * @requires vm.flagless
  * @enablePreview
  * @modules java.base/jdk.internal.misc
- * @run main/othervm -XX:+UseZGC -Xmx50M -Xlog:gc*=info runtime.valhalla.inlinetypes.LarvalMarkWordTest
+            java.management
+ * @build jdk.test.whitebox.WhiteBox
+ * @run driver jdk.test.lib.helpers.ClassFileInstaller jdk.test.whitebox.WhiteBox
+ * @run main/othervm -XX:+UseZGC -Xlog:gc*=info
+                     -Xbootclasspath/a:. -XX:+UnlockDiagnosticVMOptions -XX:+WhiteBoxAPI
+                     runtime.valhalla.inlinetypes.LarvalMarkWordTest
  */
-
 package runtime.valhalla.inlinetypes;
+
+import jdk.test.whitebox.WhiteBox;
 
 import jdk.internal.misc.Unsafe;
 
 public class LarvalMarkWordTest {
   private static final Unsafe UNSAFE = Unsafe.getUnsafe();
+  private static final WhiteBox WB = WhiteBox.getWhiteBox();
 
   // We want to ensure that the larval bit in the markWord is preserved
   // accross GC events.
@@ -71,12 +100,10 @@ public class LarvalMarkWordTest {
     val = UNSAFE.makePrivateBuffer(val);
     // Write a new value to it.
     UNSAFE.putLong(val, offset, 19);
-    // Now we make a bunch of objects.
-    // Ideally, we want the object to be relocated, but this isn't guaranteed.
-    stressTheGC();
-    // Hopefully the GC has already decided to do a GC, but if not we
-    // suggest it do one. This suggestion may be ignored.
-    System.gc();
+
+    // Explicitly cause a full GC.
+    // This will cause relocations to happen, which is what we want to test.
+    WB.fullGC();
 
     // At this point, the GC's preservation should have seen that val was
     // a larval, and if it did anything with val, preserved the markWord.
@@ -87,20 +114,6 @@ public class LarvalMarkWordTest {
       throw new IllegalStateException("something went wrong during larval construction");
     }
   }
-
-  private static void stressTheGC() {
-    int ignore = 0;
-    for (int i = 0; i < 100_000_000; i++) {
-      Object obj = new Object();
-      // Arbitrary operation, has no meaning.
-      ignore %= obj.getClass().getName().length();
-    }
-    if (ignore == -1) {
-      // Will never be thrown.
-      throw new RuntimeException("will never throw");
-    }
-  }
-
 
   private static value class MyValue {
     private final int x;
