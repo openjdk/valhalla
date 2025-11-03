f9a93d31a1fbd440ed63a8fa4d3ff29bfa9b1ef1
Paul Hübner <paul.hubner@oracle.com>
2025-11-03 15:44:21 +0100

    Improve flat array copying test.

--- test/hotspot/jtreg/runtime/valhalla/inlinetypes/FlatArrayCopyingTest.java
+++ test/hotspot/jtreg/runtime/valhalla/inlinetypes/FlatArrayCopyingTest.java
@@ -1,5 +1,4 @@
 /*
-/*
  * Copyright (c) 2025, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
@@ -24,27 +23,74 @@
  */
 
 /*
- * @test
- * @summary Ensures that value arrays can get arraycopied properly.
+ * @test id=serial
+ * @summary Ensures that value arrays can get arraycopied properly with Serial.
  *          This test will likely crash if that is not the case.
  * @bug 8370479
  * @enablePreview
  * @requires vm.flagless
+ * @library /test/lib /
  * @modules java.base/jdk.internal.value
- * @compile FlatArrayCopyingTest.java
+            java.management
+ * @build jdk.test.whitebox.WhiteBox
+ * @run driver jdk.test.lib.helpers.ClassFileInstaller jdk.test.whitebox.WhiteBox
+ * @run junit/othervm/timeout=480 -Xint -XX:+UseSerialGC -XX:+UseCompressedOops -Xlog:gc*=info
+                                  -XX:+UseCompressedClassPointers
+                                  -Xbootclasspath/a:. -XX:+UnlockDiagnosticVMOptions -XX:+WhiteBoxAPI
+                                  runtime.valhalla.inlinetypes.FlatArrayCopyingTest
+ */
+
+/*
+ * @test id=parallel
+ * @summary Ensures that value arrays can get arraycopied properly with Parallel.
+ *          This test will likely crash if that is not the case.
+ * @bug 8370479
+ * @enablePreview
+ * @requires vm.flagless
+ * @library /test/lib /
+ * @modules java.base/jdk.internal.value
+            java.management
+ * @build jdk.test.whitebox.WhiteBox
+ * @run driver jdk.test.lib.helpers.ClassFileInstaller jdk.test.whitebox.WhiteBox
+ * @run junit/othervm/timeout=480 -Xint -XX:+UseParallelGC -XX:+UseCompressedOops -Xlog:gc*=info
+                                  -XX:ParallelGCThreads=1 -XX:-UseDynamicNumberOfGCThreads
+                                  -Xbootclasspath/a:. -XX:+UnlockDiagnosticVMOptions -XX:+WhiteBoxAPI
+                                  runtime.valhalla.inlinetypes.FlatArrayCopyingTest
+ */
+
+/*
+ * @test id=g1
+ * @summary Ensures that value arrays can get arraycopied properly with G1.
+ *          This test will likely crash if that is not the case.
+ * @bug 8370479
+ * @enablePreview
+ * @requires vm.flagless
+ * @library /test/lib /
+ * @modules java.base/jdk.internal.value
+            java.management
+ * @build jdk.test.whitebox.WhiteBox
+ * @run driver jdk.test.lib.helpers.ClassFileInstaller jdk.test.whitebox.WhiteBox
  * @run junit/othervm/timeout=480 -Xint -XX:+UseG1GC -XX:+UseCompressedOops -Xlog:gc*=info
                                   -XX:ParallelGCThreads=1 -XX:ConcGCThreads=1 -XX:-UseDynamicNumberOfGCThreads
-                                  -XX:-G1UseConcRefinement -XX:+UseCompressedClassPointers FlatArrayCopyingTest
+                                  -XX:-G1UseConcRefinement -XX:+UseCompressedClassPointers
+                                  -Xbootclasspath/a:. -XX:+UnlockDiagnosticVMOptions -XX:+WhiteBoxAPI
+                                  runtime.valhalla.inlinetypes.FlatArrayCopyingTest
  */
+
+package runtime.valhalla.inlinetypes;
 
 import java.util.Arrays;
 import jdk.internal.value.ValueClass;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import jdk.test.whitebox.WhiteBox;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public final class FlatArrayCopyingTest {
+    private static final WhiteBox WB = WhiteBox.getWhiteBox();
+
     public static value record Element(Identity underlying) {}
     public static class Identity {}
 
@@ -54,6 +100,10 @@ public final class FlatArrayCopyingTest {
         for (int i = 0; i < 1_000_000; i++) {
             if (i == array.length) {
                 array = Arrays.copyOf(array, array.length << 1);
+            }
+            // Do a very "random" full GC.
+            if (i == 5123123) {
+                WB.fullGC();
             }
             array[i] = new Element(new Identity());
         }
