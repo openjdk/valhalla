296fe862f73ad92093d62372141dc848a3e42d72
Alex Menkov <amenkov@openjdk.org>
2025-11-07 19:27:40 +0000

    8341771: [lworld] Heap dump recognizing new flat fields formats 8328468: [lworld] Reimplement HeapDump test for JEP401 8317416: [lworld] JDK-8306441 (Two phase segmented heap dump) needs Valhalla support
    Reviewed-by: fparain

--- test/lib/jdk/test/lib/hprof/model/JavaObject.java
+++ test/lib/jdk/test/lib/hprof/model/JavaObject.java
@@ -42,12 +42,6 @@ public class JavaObject extends JavaLazyReadObject {
 
     private Object clazz;       // Number before resolve
                                 // JavaClass after resolve
-
-    public JavaObject(Object clazz, long offset) {
-        super(offset);
-        this.clazz = clazz;
-    }
-
     /**
      * Construct a new JavaObject.
      *
@@ -55,7 +49,8 @@ public class JavaObject extends JavaLazyReadObject {
      * @param offset The offset of field data
      */
     public JavaObject(long classID, long offset) {
-        this(makeId(classID), offset);
+        super(offset);
+        this.clazz = makeId(classID);
     }
 
     public void resolve(Snapshot snapshot) {
@@ -221,7 +216,7 @@ public class JavaObject extends JavaLazyReadObject {
      *     byte[length]
      */
     @Override
-    protected long readValueLength() throws IOException {
+    protected final long readValueLength() throws IOException {
         long lengthOffset = getOffset() + 2 * idSize() + 4;
         return buf().getInt(lengthOffset);
     }
@@ -231,7 +226,7 @@ public class JavaObject extends JavaLazyReadObject {
         return parseFields(false);
     }
 
-    protected long dataStartOffset() {
+    private long dataStartOffset() {
         return getOffset() + idSize() + 4 + idSize() + 4;
     }
 
@@ -265,77 +260,71 @@ public class JavaObject extends JavaLazyReadObject {
             JavaField f = fields[fieldNo];
             char sig = f.getSignature().charAt(0);
             try {
-                if (f instanceof InlinedJavaField inlinedField) {
-                    JavaClass fieldClass = inlinedField.getInlinedFieldClass();
-                    fieldValues[target+fieldNo] = new InlinedJavaObject(fieldClass, offset);
-                    offset += fieldClass.getInlinedInstanceSize();
-                } else {
-                    switch (sig) {
-                        case 'L':
-                        case '[': {
-                            long id = objectIdAt(offset);
-                            offset += idSize();
-                            JavaObjectRef ref = new JavaObjectRef(id);
-                            fieldValues[target + fieldNo] = ref.dereference(snapshot, f, verbose);
-                            break;
-                        }
-                        case 'Z': {
-                            byte value = byteAt(offset);
-                            offset++;
-                            fieldValues[target + fieldNo] = new JavaBoolean(value != 0);
-                            break;
-                        }
-                        case 'B': {
-                            byte value = byteAt(offset);
-                            offset++;
-                            fieldValues[target + fieldNo] = new JavaByte(value);
-                            break;
-                        }
-                        case 'S': {
-                            short value = shortAt(offset);
-                            offset += 2;
-                            fieldValues[target + fieldNo] = new JavaShort(value);
-                            break;
-                        }
-                        case 'C': {
-                            char value = charAt(offset);
-                            offset += 2;
-                            fieldValues[target + fieldNo] = new JavaChar(value);
-                            break;
-                        }
-                        case 'I': {
-                            int value = intAt(offset);
-                            offset += 4;
-                            fieldValues[target + fieldNo] = new JavaInt(value);
-                            break;
-                        }
-                        case 'J': {
-                            long value = longAt(offset);
-                            offset += 8;
-                            fieldValues[target + fieldNo] = new JavaLong(value);
-                            break;
-                        }
-                        case 'F': {
-                            float value = floatAt(offset);
-                            offset += 4;
-                            fieldValues[target + fieldNo] = new JavaFloat(value);
-                            break;
-                        }
-                        case 'D': {
-                            double value = doubleAt(offset);
-                            offset += 8;
-                            fieldValues[target + fieldNo] = new JavaDouble(value);
-                            break;
-                        }
-                        default:
-                            throw new RuntimeException("invalid signature: " + sig);
-
+                switch (sig) {
+                    case 'L':
+                    case '[': {
+                        long id = objectIdAt(offset);
+                        offset += idSize();
+                        JavaObjectRef ref = new JavaObjectRef(id);
+                        fieldValues[target+fieldNo] = ref.dereference(snapshot, f, verbose);
+                        break;
                     }
+                    case 'Z': {
+                        byte value = byteAt(offset);
+                        offset++;
+                        fieldValues[target+fieldNo] = new JavaBoolean(value != 0);
+                        break;
+                    }
+                    case 'B': {
+                        byte value = byteAt(offset);
+                        offset++;
+                        fieldValues[target+fieldNo] = new JavaByte(value);
+                        break;
+                    }
+                    case 'S': {
+                        short value = shortAt(offset);
+                        offset += 2;
+                        fieldValues[target+fieldNo] = new JavaShort(value);
+                        break;
+                    }
+                    case 'C': {
+                        char value = charAt(offset);
+                        offset += 2;
+                        fieldValues[target+fieldNo] = new JavaChar(value);
+                        break;
+                    }
+                    case 'I': {
+                        int value = intAt(offset);
+                        offset += 4;
+                        fieldValues[target+fieldNo] = new JavaInt(value);
+                        break;
+                    }
+                    case 'J': {
+                        long value = longAt(offset);
+                        offset += 8;
+                        fieldValues[target+fieldNo] = new JavaLong(value);
+                        break;
+                    }
+                    case 'F': {
+                        float value = floatAt(offset);
+                        offset += 4;
+                        fieldValues[target+fieldNo] = new JavaFloat(value);
+                        break;
+                    }
+                    case 'D': {
+                        double value = doubleAt(offset);
+                        offset += 8;
+                        fieldValues[target+fieldNo] = new JavaDouble(value);
+                        break;
+                    }
+                    default:
+                        throw new RuntimeException("invalid signature: " + sig);
+
                 }
-            } catch (IOException exp) {
-                System.err.println("lazy read failed at offset " + offset);
-                exp.printStackTrace();
-                return Snapshot.EMPTY_JAVATHING_ARRAY;
+        } catch (IOException exp) {
+            System.err.println("lazy read failed at offset " + offset);
+            exp.printStackTrace();
+            return Snapshot.EMPTY_JAVATHING_ARRAY;
             }
         }
         return fieldValues;
