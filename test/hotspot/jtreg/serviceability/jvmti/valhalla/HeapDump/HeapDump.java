fe54fe5cd7193ee77fa53879eefcd7ea1b5af1d1
Paul Hübner <paul.hubner@oracle.com>
2025-11-26 10:03:25 +0100

    Add diagnostic flag to tests.

--- test/hotspot/jtreg/serviceability/jvmti/valhalla/HeapDump/HeapDump.java
+++ test/hotspot/jtreg/serviceability/jvmti/valhalla/HeapDump/HeapDump.java
@@ -149,9 +149,9 @@ public class HeapDump {
         try {
             theApp = new HeapDumpTarg();
 
-            LingeredApp.startApp(theApp, "--enable-preview",
-                                 "-XX:+UnlockDiagnosticVMOptions",
-                                 "-XX:+PrintInlineLayout", "-XX:+PrintFlatArrayLayout",
+            // -XX:+PrintInlineLayout is debug-only arg
+            LingeredApp.startApp(theApp, "--enable-preview", "-XX:+UnlockDiagnosticVMOptions",
+                                "-XX:+PrintInlineLayout", "-XX:+PrintFlatArrayLayout",
                                  "--add-modules=java.base",
                                  "--add-exports=java.base/jdk.internal.value=ALL-UNNAMED");
 
