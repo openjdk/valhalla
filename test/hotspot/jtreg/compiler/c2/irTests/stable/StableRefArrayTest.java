79091092806acdd8324b6dbcba93c7db7326778b
Marc Chevalier <marc.chevalier@oracle.com>
2025-12-11 15:05:38 +0100

    applyIf

--- test/hotspot/jtreg/compiler/c2/irTests/stable/StableRefArrayTest.java
+++ test/hotspot/jtreg/compiler/c2/irTests/stable/StableRefArrayTest.java
@@ -112,7 +112,8 @@ public class StableRefArrayTest {
 
     @Test
     @IR(counts = { IRNode.LOAD, ">0" })
-    @IR(failOn = { IRNode.MEMBAR })
+    @IR(applyIf = {"enable-valhalla", "false"}, failOn = { IRNode.MEMBAR })
+    @IR(applyIf = {"enable-valhalla", "true"}, counts = { IRNode.MEMBAR, "> 0" })
     static int testPartialFold() {
         // Access should not be folded.
         // No barriers expected for plain fields.
