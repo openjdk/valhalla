d408d01f0a415f9f779c5b7e01ea2499052ff26e
Stefan Karlsson <stefank@openjdk.org>
2025-12-16 20:41:35 +0000

    8373779: [lworld] Type confusion with InlineKlassFixedBlock fields
    Reviewed-by: thartmann, coleenp, fparain

--- src/hotspot/share/classfile/classFileParser.cpp
+++ src/hotspot/share/classfile/classFileParser.cpp
@@ -5351,9 +5351,6 @@ void ClassFileParser::fill_instance_klass(InstanceKlass* ik,
   // this transfers ownership of a lot of arrays from
   // the parser onto the InstanceKlass*
   apply_parsed_class_metadata(ik, _java_fields_count);
-  if (ik->is_inline_klass()) {
-    InlineKlass::cast(ik)->init_fixed_block();
-  }
 
   // can only set dynamic nest-host after static nest information is set
   if (cl_inst_info.dynamic_nest_host() != nullptr) {
