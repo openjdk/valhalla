49ebfd7942a5d63d675d39de8be3a2383e883416
Matias Saavedra Silva <matias.s.saavedra.silva@oracle.com>
2025-12-16 13:37:00 -0500

    Reverted preview patching flag

--- src/hotspot/share/runtime/arguments.cpp
+++ src/hotspot/share/runtime/arguments.cpp
@@ -2087,7 +2087,7 @@ int Arguments::process_patch_mod_option(const char* patch_mod_tail) {
 // Temporary system property to disable preview patching and enable the new preview mode
 // feature for testing/development. Once the preview mode feature is finished, the value
 // will be always 'true' and this code, and all related dead-code can be removed.
-#define DISABLE_PREVIEW_PATCHING_DEFAULT true
+#define DISABLE_PREVIEW_PATCHING_DEFAULT false
 
 bool Arguments::disable_preview_patching() {
   const char* prop = get_property("DISABLE_PREVIEW_PATCHING");
