59f199e758ba1c28c52ba52058f8bc6cef15d7de
Maurizio Cimadamore <maurizio.cimadamore@oracle.com>
2026-01-09 11:20:22 +0000

    Initial push

---
+++ test/langtools/tools/javac/nullability/NullRestrictedArrayCreationTest.java
@@ -0,0 +1,25 @@
+/*
+ * @test /nodynamiccopyright/
+ * @summary Smoke test for null-restricted array creation type-checking
+ * @enablePreview
+ * @compile/fail/ref=NullRestrictedArrayCreationTest.out -XDrawDiagnostics NullRestrictedArrayCreationTest.java
+ */
+
+public class NullRestrictedArrayCreationTest {
+    void testSimple(int n) {
+        var t1 = new String![10];             // Fail (no init, n > 0)
+        var t2 = new String![0];              // Pass (no init, n == 0)
+        var t3 = new String![] { "x", "y" };  // Pass (init)
+        var t21 = new String![n];             // Fail (no init, n not constant)
+    }
+
+    void testMulti(int n) {
+        var t6 = new String![3][2];           // Fail (no init, innermost length > 0)
+        var t7 = new String![3][0];           // Pass (no init, innermost length == 0)
+        var t8 = new String![0][10];          // Pass (no init, outer length == 0, no array created)
+        var t9 = new String![][] { {"x"} };   // Pass (init)
+        var t10 = new String![10][];          // Pass (no init, no innermost length)
+        var t12 = new String![2][][];         // Pass (no init, no innermost length)
+        var t22 = new String![3][n];          // Fail (no init, innermost length not constant)
+    }
+}
