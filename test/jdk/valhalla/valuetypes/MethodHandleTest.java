c259bd40ebf1a37d607411282202d579fbd3c593
Chen Liang <liach@openjdk.org>
2026-01-27 15:06:49 +0000

    8376033: [lworld] Remove auto early construction from strict fields
    Reviewed-by: vromero

--- test/jdk/valhalla/valuetypes/MethodHandleTest.java
+++ test/jdk/valhalla/valuetypes/MethodHandleTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018, 2025, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2018, 2026, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -80,6 +80,7 @@ public class MethodHandleTest {
         Ref(Point p, Line l) {
             this.p = p;
             this.l = l;
+            super();
         }
     }
 
--- test/micro/org/openjdk/bench/valhalla/invoke/field/ValueNullFree.java
+++ test/micro/org/openjdk/bench/valhalla/invoke/field/ValueNullFree.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2025, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2025, 2026, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -111,6 +111,7 @@ public class ValueNullFree {
 
         public ValWrapper(ValueInt0 f) {
             this.f = f;
+            super();
         }
     }
 
