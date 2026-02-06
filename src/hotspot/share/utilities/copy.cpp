eae0cd4462dfe3f2f4fe552ce60197d9f1c9d13f
Axel Boldt-Christmas <axel.boldt-christmas@oracle.com>
2026-02-06 12:58:01 +0000

    Fix Copy::copy_value_content

--- src/hotspot/share/utilities/copy.cpp
+++ src/hotspot/share/utilities/copy.cpp
@@ -53,41 +53,33 @@ void Copy::conjoint_memory_atomic(const void* from, void* to, size_t size) {
   }
 }
 
-#define COPY_ALIGNED_SEGMENT(t) \
-  if (bits % sizeof(t) == 0) { \
-    size_t segment = remain / sizeof(t); \
-    if (segment > 0) { \
-      Copy::conjoint_##t##s_atomic((const t*) cursor_from, (t*) cursor_to, segment); \
-      remain -= segment * sizeof(t); \
-      cursor_from = (void*)(((char*)cursor_from) + segment * sizeof(t)); \
-      cursor_to = (void*)(((char*)cursor_to) + segment * sizeof(t)); \
-    } \
-  } \
-
-void Copy::copy_value_content(const void* from, void* to, size_t size) {
-  // Simple cases first
-  uintptr_t bits = (uintptr_t) from | (uintptr_t) to | (uintptr_t) size;
-  if (bits % sizeof(jlong) == 0) {
-    Copy::conjoint_jlongs_atomic((const jlong*) from, (jlong*) to, size / sizeof(jlong));
-    return;
-  } else if (bits % sizeof(jint) == 0) {
-    Copy::conjoint_jints_atomic((const jint*) from, (jint*) to, size / sizeof(jint));
-    return;
-  } else if (bits % sizeof(jshort) == 0) {
-    Copy::conjoint_jshorts_atomic((const jshort*) from, (jshort*) to, size / sizeof(jshort));
-    return;
+#define COPY_ALIGNED_SEGMENT(t)                                                \
+  if (bits % sizeof(t) == 0) {                                                 \
+    size_t segment = remaining_bytes / sizeof(t);                              \
+    if (segment > 0) {                                                         \
+      Copy::conjoint_##t##s_atomic((const t*)cursor_from, (t*)cursor_to,       \
+                                   segment);                                   \
+      remaining_bytes -= segment * sizeof(t);                                  \
+      cursor_from = (void*)(((char*)cursor_from) + segment * sizeof(t));       \
+      cursor_to = (void*)(((char*)cursor_to) + segment * sizeof(t));           \
+    }                                                                          \
   }
 
-  // Complex cases
-  bits = (uintptr_t) from | (uintptr_t) to;
+void Copy::copy_value_content(const void* from, void* to, size_t size) {
+  // The alignment of the containing object must satisfy the alignment of all
+  // its fields, as such we can safely copy atomically starting at the largest
+  // atomic size which the payloads are aligned to. Any trailing payload smaller
+  // than this atomic size must have a lower alignment requirement. This
+  // property holds recursively down to the smallest atomic size.
+  const uintptr_t bits = (uintptr_t)from | (uintptr_t)to;
   const void* cursor_from = from;
   void* cursor_to = to;
-  size_t remain = size;
+  size_t remaining_bytes = size;
   COPY_ALIGNED_SEGMENT(jlong)
   COPY_ALIGNED_SEGMENT(jint)
   COPY_ALIGNED_SEGMENT(jshort)
-  if (remain > 0) {
-    Copy::conjoint_jbytes((const void*) cursor_from, (void*) cursor_to, remain);
+  if (remaining_bytes > 0) {
+    Copy::conjoint_jbytes(cursor_from, cursor_to, remaining_bytes);
   }
 }
 
