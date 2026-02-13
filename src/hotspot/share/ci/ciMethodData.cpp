42197b2d459cb689180ba8a2dab21fa773ce93dd
Christian Hagedorn <chagedorn@openjdk.org>
2026-02-13 07:27:02 +0000

    8377792: [lworld] compiler/ciReplay/TestLambdas.java fails
    Reviewed-by: thartmann

--- src/hotspot/share/ci/ciMethodData.cpp
+++ src/hotspot/share/ci/ciMethodData.cpp
@@ -722,8 +722,8 @@ void ciMethodData::dump_replay_data_type_helper(outputStream* out, int round, in
     if (round == 0) {
       count++;
     } else {
-      if (Arguments::is_valhalla_enabled() && k->is_obj_array_klass()) {
-        // For value class arrays, we also record the array property to load the correct array class during replay.
+      if (k->is_obj_array_klass()) {
+        // We also record the array property to load the correct array class during replay compilation.
         ArrayKlass::ArrayProperties array_properties = k->as_obj_array_klass()->properties();
         out->print(" %d %s %d", static_cast<int>(dp_to_di(pdata->dp() + in_bytes(offset)) / sizeof(intptr_t)),
                                       CURRENT_ENV->replay_name(k), static_cast<int>(array_properties));
