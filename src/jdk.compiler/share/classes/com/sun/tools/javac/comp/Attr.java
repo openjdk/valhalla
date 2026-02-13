91cda3f85a2245ef533a7d40597e12cf0c21c219
Vicente Romero <vicente.romero@oracle.com>
2026-02-13 09:29:57 -0500

    review comments

--- src/jdk.compiler/share/classes/com/sun/tools/javac/comp/Attr.java
+++ src/jdk.compiler/share/classes/com/sun/tools/javac/comp/Attr.java
@@ -4008,13 +4008,16 @@ public class Attr extends JCTree.Visitor {
                 checkContext.report(tree, diags.fragment(Fragments.IncompatibleArgTypesInLambda));
             } else if (allowNullRestrictedTypes) {
                 // ok they are equal but what about nullability?
-                for (ArgsNullabilityResult incompatibleParam :
-                        chk.checkArgsNullability(lambdaTypes, argTypes, tree.params)) {
-                            chk.warnNullableTypes(incompatibleParam.position() != null ?
-                                            incompatibleParam.position().vartype : tree,
+                boolean isSpeculativeRound = checkContext.deferredAttrContext().mode == DeferredAttr.AttrMode.SPECULATIVE;
+                if (!isSpeculativeRound) {
+                    for (ArgsNullabilityResult incompatibleParam :
+                            chk.checkArgsNullability(lambdaTypes, argTypes, tree.params)) {
+                        chk.warnNullableTypes(incompatibleParam.position() != null ?
+                                        incompatibleParam.position().vartype : tree,
                                 LintWarnings.IncompatibleNullRestrictions(
                                         Fragments.LambdaArgumentTypeNullabilityMismatch(incompatibleParam.overridingType(),
                                                 incompatibleParam.overridenType(), tree.target)));
+                    }
                 }
             }
         }
