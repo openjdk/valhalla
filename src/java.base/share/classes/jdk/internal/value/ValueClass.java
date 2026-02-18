d5e3620d5a0b13cce9c19bafd5b2dd881b8bf9e9
Paul Hübner <phubner@openjdk.org>
2026-02-18 08:53:00 +0000

    8377576: [lworld] JVM_CopyOfSpecialArray does not handle all inputs correctly
    Reviewed-by: liach

--- src/java.base/share/classes/jdk/internal/value/ValueClass.java
+++ src/java.base/share/classes/jdk/internal/value/ValueClass.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023, 2025, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2023, 2026, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -90,7 +90,21 @@ public final class ValueClass {
     @IntrinsicCandidate
     public static native boolean isFlatArray(Object array);
 
-    public static Object[] copyOfSpecialArray(Object[] array, int from, int to) {
+    public static Object[] copyOfSpecialArray(Object[] array, int newLength) {
+        if (newLength < 0) {
+            throw new NegativeArraySizeException("" + newLength);
+        }
+        return copyOfSpecialArray0(array, 0, newLength);
+    }
+
+    public static Object[] copyOfRangeSpecialArray(Object[] array, int from, int to) {
+        int length = array.length;
+        if (from < 0 || from > length) {
+            throw new ArrayIndexOutOfBoundsException("source index " + from + " out of bounds for object array[" + length + "]");
+        }
+        if (from > to) {
+            throw new IllegalArgumentException(from + " > " + to);
+        }
         return copyOfSpecialArray0(array, from, to);
     }
 
