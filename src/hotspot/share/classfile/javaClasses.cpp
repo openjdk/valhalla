2163c22dffd0aee3ccd98814a18d75a3e419f933
David Simms <david.simms@oracle.com>
2026-02-18 14:52:37 +0100

    Tweak to 8375569, avoid creating scratch mirror for refined array klass

--- src/hotspot/share/classfile/javaClasses.cpp
+++ src/hotspot/share/classfile/javaClasses.cpp
@@ -1290,7 +1290,7 @@ bool java_lang_Class::restore_archived_mirror(Klass *k,
         "Restored %s archived mirror " PTR_FORMAT, k->external_name(), p2i(mirror()));
   }
 
-  if (CDSConfig::is_dumping_heap()) {
+  if (CDSConfig::is_dumping_heap() && (!k->is_refined_objArray_klass())) {
     create_scratch_mirror(k, CHECK_(false));
   }
 
