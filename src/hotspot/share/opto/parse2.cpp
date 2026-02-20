62b3599cfbf7697a41fd83392c36133474ec8c6d
Benoît Maillard <benoit.maillard@oracle.com>
2026-02-20 11:18:09 +0100

    Remove debug comment

--- src/hotspot/share/opto/parse2.cpp
+++ src/hotspot/share/opto/parse2.cpp
@@ -2134,7 +2134,7 @@ void Parse::do_acmp(BoolTest::mask btest, Node* left, Node* right) {
   const TypeOopPtr* tleft = _gvn.type(left)->isa_oopptr();
   const TypeOopPtr* tright = _gvn.type(right)->isa_oopptr();
   Node* cmp = CmpP(left, right);
-  record_for_igvn(cmp); // TODO it's this one
+  record_for_igvn(cmp);
   cmp = optimize_cmp_with_klass(cmp);
   if (tleft == nullptr || !tleft->can_be_inline_type() ||
       tright == nullptr || !tright->can_be_inline_type()) {
