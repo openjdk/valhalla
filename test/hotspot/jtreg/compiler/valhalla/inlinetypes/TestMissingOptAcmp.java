fa3e08490407d3b95a1a36d63bfb50f81447ed8d
Benoît Maillard <benoit.maillard@oracle.com>
2026-02-20 11:04:29 +0100

    Add test

---
+++ test/hotspot/jtreg/compiler/valhalla/inlinetypes/TestMissingOptAcmp.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright (c) 2026, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+
+package compiler.valhalla.inlinetypes;
+
+/*
+ * @test
+ * @key randomness
+ * @summary TODO.
+ * @library /test/lib /
+ * @enablePreview
+ * @modules java.base/jdk.internal.value
+ *          java.base/jdk.internal.vm.annotation
+ * @run main/othervm -Xcomp -XX:-TieredCompilation -XX:VerifyIterativeGVN=1110
+ *                   -XX:CompileCommand=compileonly,${test.main.class}::test
+ *                   ${test.main.class}
+ * @run main ${test.main.class}
+ */
+
+
+value class MyValue1NewAcmp {
+    int x;
+
+    MyValue1NewAcmp(int x) {
+        this.x = x;
+    }
+}
+
+public class TestMissingOptAcmp {
+    public static void main(String[] args) {
+        test(new MyValue1NewAcmp(1), new MyValue1NewAcmp(1));
+    }
+
+    public static Object getNotNull(Object u) {
+        return (u != null) ? u : new Object();
+    }
+
+    public static boolean test(MyValue1NewAcmp v, Object u) {
+        return (Object)v == getNotNull(u);
+    }
+}
