5aa91ddeb51bd729b30482137b0715e7f5df7e52
Axel Boldt-Christmas <axel.boldt-christmas@oracle.com>
2026-03-05 13:51:54 +0100

    Update cardTableBarrierSet.inline.hpp

--- src/hotspot/share/gc/shared/cardTableBarrierSet.inline.hpp
+++ src/hotspot/share/gc/shared/cardTableBarrierSet.inline.hpp
@@ -222,8 +222,8 @@ value_store_null_in_heap(const ValuePayload& dst) {
     const address oop_map_adjusted_dst_addr = dst.addr() - md->payload_offset();
     typedef typename ValueOopType<decorators>::type OopType;
 
+    // Pre-barriers...
     if (!HasDecorator<decorators, IS_DEST_UNINITIALIZED>::value) {
-      // Pre-barriers...
       OopMapBlock* map = md->start_of_nonstatic_oop_maps();
       OopMapBlock* const end = map + md->nonstatic_oop_map_count();
       while (map != end) {
