c3ee772f7a93051450d2eeab368d17cdb311d0d2
David Holmes <david.holmes@oracle.com>
2026-03-09 03:49:51 -0400

    Fix typo in probe

--- src/hotspot/share/prims/jni.cpp
+++ src/hotspot/share/prims/jni.cpp
@@ -3175,7 +3175,7 @@ JNI_ENTRY(jboolean, jni_IsVirtualThread(JNIEnv* env, jobject obj))
 JNI_END
 
 JNI_ENTRY(jboolean, jni_IsValueObject(JNIEnv* env, jobject obj))
-  HOTSPOT_JNI_ISVALUEOBJECT_ENTRY(env, jobject obj);
+  HOTSPOT_JNI_ISVALUEOBJECT_ENTRY(env, obj);
   oop o = JNIHandles::resolve(obj);
   if (o != nullptr && o->klass()->is_inline_klass()) {
     HOTSPOT_JNI_ISVALUEOBJECT_RETURN(JNI_TRUE);
