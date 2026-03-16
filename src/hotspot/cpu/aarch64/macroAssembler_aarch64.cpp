145c2d33d31adc47bef6d9d320efe2cca438c2c5
Damon Fenacci <damon.fenacci@oracle.com>
2026-03-16 11:21:57 +0100

    JDK-8379913: aarch64

--- src/hotspot/cpu/aarch64/macroAssembler_aarch64.cpp
+++ src/hotspot/cpu/aarch64/macroAssembler_aarch64.cpp
@@ -2042,6 +2042,7 @@ void MacroAssembler::_verify_oop(Register reg, const char* s, const char* file, 
     stringStream ss;
     ss.print("verify_oop: %s: %s (%s:%d)", reg->name(), s, file, line);
     b = code_string(ss.as_string());
+    b = AOTCodeCache::add_C_string(b);
   }
   BLOCK_COMMENT("verify_oop {");
 
@@ -2078,6 +2079,7 @@ void MacroAssembler::_verify_oop_addr(Address addr, const char* s, const char* f
     stringStream ss;
     ss.print("verify_oop_addr: %s (%s:%d)", s, file, line);
     b = code_string(ss.as_string());
+    b = AOTCodeCache::add_C_string(b);
   }
   BLOCK_COMMENT("verify_oop_addr {");
 
