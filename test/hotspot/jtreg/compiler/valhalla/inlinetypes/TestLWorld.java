e06f971ec8a17a46f266922bd934b6eb2f4e89d8
Christian Hagedorn <christian.hagedorn@oracle.com>
2026-03-19 09:23:52 +0100

    re-enable assert

--- test/hotspot/jtreg/compiler/valhalla/inlinetypes/TestLWorld.java
+++ test/hotspot/jtreg/compiler/valhalla/inlinetypes/TestLWorld.java
@@ -4646,13 +4646,13 @@ public class TestLWorld {
     static final SubValueClassWithInt[] SUB_VALUE_CLASS_WITH_INT_ARRAY = (SubValueClassWithInt[]) ValueClass.newNullRestrictedNonAtomicArray(SubValueClassWithInt.class, 2, new SubValueClassWithInt(0));
     static final SubValueClassWithDouble[] SUB_VALUE_CLASS_WITH_DOUBLE_ARRAY = (SubValueClassWithDouble[]) ValueClass.newNullRestrictedNonAtomicArray(SubValueClassWithDouble.class, 2, new SubValueClassWithDouble(0));
 
-// TODO: Can only be enabled once JDK-8343835 is fixed. Otherwise, we hit the mismatched stores assert.
-//    static {
-//        VALUE_CLASS_WITH_INT_ARRAY[0] = new ValueClassWithInt(5);
-//        VALUE_CLASS_WITH_DOUBLE_ARRAY[0] = new ValueClassWithDouble(6);
-//        SUB_VALUE_CLASS_WITH_INT_ARRAY[0] = new SubValueClassWithInt(7);
-//        SUB_VALUE_CLASS_WITH_DOUBLE_ARRAY[0] = new SubValueClassWithDouble(8);
-//    }
+
+    static {
+        VALUE_CLASS_WITH_INT_ARRAY[0] = new ValueClassWithInt(5);
+        VALUE_CLASS_WITH_DOUBLE_ARRAY[0] = new ValueClassWithDouble(6);
+        SUB_VALUE_CLASS_WITH_INT_ARRAY[0] = new SubValueClassWithInt(7);
+        SUB_VALUE_CLASS_WITH_DOUBLE_ARRAY[0] = new SubValueClassWithDouble(8);
+    }
 
     @Test
     static void testFlatArrayInexactObjectStore(Object o, boolean flag) {
@@ -4716,12 +4716,6 @@ public class TestLWorld {
                  "testFlatArrayInexactAbstractValueClassStore",
                  "testFlatArrayInexactAbstractValueClassLoad"})
     static void runFlatArrayInexactLoadAndStore() {
-        // TODO: Remove these again once JDK-8343835 is fixed and uncomment static initializer above
-        VALUE_CLASS_WITH_INT_ARRAY[0] = new ValueClassWithInt(5);
-        VALUE_CLASS_WITH_DOUBLE_ARRAY[0] = new ValueClassWithDouble(6);
-        SUB_VALUE_CLASS_WITH_INT_ARRAY[0] = new SubValueClassWithInt(7);
-        SUB_VALUE_CLASS_WITH_DOUBLE_ARRAY[0] = new SubValueClassWithDouble(8);
-
         boolean flag = true;
         ValueClassWithInt valueClassWithInt = new ValueClassWithInt(15);
         ValueClassWithDouble valueClassWithDouble = new ValueClassWithDouble(16);
