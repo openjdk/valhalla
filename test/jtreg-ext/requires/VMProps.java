51d4356be885e35aca88fe79eedd864024ad8fd3
Paul Hübner <paul.hubner@oracle.com>
2026-03-24 09:51:38 +0100

    Revert "Constrain hasSA to non-preview configurations."
    This reverts commit 7bdfce0b642447580ad3da2c4d7fb3abd95a9a8c.

--- test/jtreg-ext/requires/VMProps.java
+++ test/jtreg-ext/requires/VMProps.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016, 2026, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2016, 2025, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -418,9 +418,7 @@ public class VMProps implements Callable<Map<String, String>> {
      * @return "true" if VM has a serviceability agent.
      */
     protected String vmHasSA() {
-        // SA is not enabled for the Valhalla preview,
-        boolean isPreview = PreviewFeatures.isEnabled();
-        return "" + (Platform.hasSA() && !isPreview);
+        return "" + Platform.hasSA();
     }
 
     /**
