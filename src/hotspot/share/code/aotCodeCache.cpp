8588e5318324c8946b9ca8ff65d8e1e350bc363b
Damon Fenacci <damon.fenacci@oracle.com>
2026-03-26 10:48:53 +0100

    JDK-8379913: disable adapters caching if VerifyOops

--- src/hotspot/share/code/aotCodeCache.cpp
+++ src/hotspot/share/code/aotCodeCache.cpp
@@ -188,14 +188,13 @@ void AOTCodeCache::initialize() {
   FLAG_SET_ERGO(AOTStubCaching, false);
 
   if (VerifyOops) {
-    // Disable AOT stubs caching when VerifyOops flag is on.
+    // Disable AOT stubs and adapter caching when VerifyOops flag is on.
     // Verify oops code generated a lot of C strings which overflow
     // AOT C string table (which has fixed size).
     // AOT C string table will be reworked later to handle such cases.
-    //
-    // Note: AOT adapters are not affected - they don't have oop operations.
-    log_info(aot, codecache, init)("AOT Stubs Caching is not supported with VerifyOops.");
+    log_info(aot, codecache, init)("AOT Stub and Adater Caching is not supported with VerifyOops.");
     FLAG_SET_ERGO(AOTStubCaching, false);
+    FLAG_SET_ERGO(AOTAdapterCaching, false);
   }
 
   bool is_dumping = false;
