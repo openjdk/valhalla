4447566e3db587ef9f3f551f7251000b9e98502d
Casper Norrbin <casper.norrbin@oracle.com>
2026-04-02 14:37:29 +0200

    test improvements

--- test/hotspot/jtreg/runtime/valhalla/inlinetypes/InlineTypeDensity.java
+++ test/hotspot/jtreg/runtime/valhalla/inlinetypes/InlineTypeDensity.java
@@ -24,12 +24,12 @@
 
 package runtime.valhalla.inlinetypes;
 
+import jdk.internal.misc.Unsafe;
 import jdk.internal.value.ValueClass;
 import jdk.internal.vm.annotation.LooselyConsistentValue;
 import jdk.internal.vm.annotation.NullRestricted;
 import jdk.test.lib.Asserts;
 import jdk.test.whitebox.WhiteBox;
-import jdk.internal.misc.Unsafe;
 
 
 /**
@@ -257,6 +257,12 @@ public class InlineTypeDensity {
         Asserts.assertLessThan(flatArraySize, objectArraySize, "Flat array accounts for more heap than object array + elements !");
     }
 
+    private static void assertFlat(String name, Object[] value, boolean shouldBeFlat) {
+        String expectation = shouldBeFlat ? "be flat" : "not be flat";
+        Asserts.assertEquals(ValueClass.isFlatArray(value), shouldBeFlat,
+                "%s array should %s".formatted(name, expectation));
+    }
+
     @LooselyConsistentValue
     static value class MyByte  { byte  v = 0; }
     @LooselyConsistentValue
@@ -274,10 +280,10 @@ public class InlineTypeDensity {
             MyInt[] mia = (MyInt[])ValueClass.newNullRestrictedNonAtomicArray(MyInt.class, testSize, new MyInt());
             MyLong[] mla = (MyLong[])ValueClass.newNullRestrictedNonAtomicArray(MyLong.class, testSize, new MyLong());
 
-            Asserts.assertEquals(ValueClass.isFlatArray(mba), ARRAY_FLATTENING_ON, "MyByte array should be flat");
-            Asserts.assertEquals(ValueClass.isFlatArray(msa), ARRAY_FLATTENING_ON, "MyShort array should be flat");
-            Asserts.assertEquals(ValueClass.isFlatArray(mia), ARRAY_FLATTENING_ON, "MyInt array should be flat");
-            Asserts.assertEquals(ValueClass.isFlatArray(mla), ARRAY_FLATTENING_ON, "MyLong array should be flat");
+            assertFlat("MyByte", mba, ARRAY_FLATTENING_ON);
+            assertFlat("MyShort", msa, ARRAY_FLATTENING_ON);
+            assertFlat("MyInt", mia, ARRAY_FLATTENING_ON);
+            assertFlat("MyLong", mla, ARRAY_FLATTENING_ON);
 
             Asserts.assertTrue(UNSAFE.arrayInstanceBaseOffset(mba) % 8 == 0);
             Asserts.assertTrue(UNSAFE.arrayInstanceBaseOffset(msa) % 8 == 0);
@@ -303,10 +309,10 @@ public class InlineTypeDensity {
             MySI[] msi = (MySI[])ValueClass.newNullRestrictedNonAtomicArray(MySI.class, testSize, new MySI());
             MySSI[] mssi = (MySSI[])ValueClass.newNullRestrictedNonAtomicArray(MySSI.class, testSize, new MySSI());
 
-            Asserts.assertEquals(ValueClass.isFlatArray(mbb), ARRAY_FLATTENING_ON, "MyBB array should be flat");
-            Asserts.assertEquals(ValueClass.isFlatArray(mbs), ARRAY_FLATTENING_ON, "MyBS array should be flat");
-            Asserts.assertEquals(ValueClass.isFlatArray(msi), ARRAY_FLATTENING_ON, "MySI array should be flat");
-            Asserts.assertEquals(ValueClass.isFlatArray(mssi), ARRAY_FLATTENING_ON, "MySSI array should be flat");
+            assertFlat("MyBB", mbb, ARRAY_FLATTENING_ON);
+            assertFlat("MyBS", mbs, ARRAY_FLATTENING_ON);
+            assertFlat("MySI", msi, ARRAY_FLATTENING_ON);
+            assertFlat("MySSI", mssi, ARRAY_FLATTENING_ON);
 
             Asserts.assertTrue(UNSAFE.arrayInstanceBaseOffset(mbb) % 8 == 0);
             Asserts.assertTrue(UNSAFE.arrayInstanceBaseOffset(mbs) % 8 == 0);
