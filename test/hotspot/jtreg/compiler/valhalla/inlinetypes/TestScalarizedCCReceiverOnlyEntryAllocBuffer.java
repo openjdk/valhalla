c99b000d086fc6051f0499b36f94a6ee3822cdc8
rwestrel <rwestrel@redhat.com>
2026-04-02 15:52:27 +0200

    test

---
+++ test/hotspot/jtreg/compiler/valhalla/inlinetypes/TestScalarizedCCReceiverOnlyEntryAllocBuffer.java
@@ -0,0 +1,121 @@
+/*
+ * Copyright (c) 2026 IBM Corporation. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+
+/**
+ * @test
+ * @bug 8381563
+ * @summary [lworld] SharedRuntime::allocate_inline_types hits "buffer not of expected class" assert
+ * @enablePreview
+ * @requires (os.simpleArch == "x64" | os.simpleArch == "aarch64")
+ * @run main/othervm -Xbootclasspath/a:. -XX:+UnlockDiagnosticVMOptions -XX:+WhiteBoxAPI ${test.main.class}
+ */
+
+package compiler.valhalla.inlinetypes;
+
+public class TestScalarizedCCReceiverOnlyEntryAllocBuffer {
+
+    static Object field1;
+    static Object field2;
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 20_000; i++) {
+            test1(0);
+            test1(1);
+            test1(2);
+        }
+    }
+
+    static void test1(int flag) {
+        I i;
+        if (flag == 0) {
+            MyValue2 v2 = new MyValue2(42, 42);
+            field2 = v2;
+            i = v2;
+        } else if (flag == 1) {
+            MyValue3 v3 = new MyValue3(42, 42);
+            field2 = v3;
+            i = v3;
+        } else {
+            MyValue4 v4 = new MyValue4(42, 42);
+            field2 = v4;
+            i = v4;
+        }
+        MyValue1 v1 = new MyValue1(42);
+        field1 = v1;
+        
+        i.m(v1);
+    }
+
+    
+    static value class MyValue1 {
+        int intField;
+
+        MyValue1(int intField) {
+            this.intField = intField;
+        }
+    }
+
+    static interface I {
+        default public void m(MyValue1 v) {
+        }
+    }
+
+    static value class MyValue2 implements I {
+        int intField1;
+        int intField2;
+
+        MyValue2(int intField1, int intField2) {
+            this.intField1 = intField1;
+            this.intField2 = intField2;
+        }
+
+        public void m(MyValue1 v) {
+        }
+    }
+
+    static value class MyValue3 implements I {
+        int intField1;
+        int intField2;
+
+        MyValue3(int intField1, int intField2) {
+            this.intField1 = intField1;
+            this.intField2 = intField2;
+        }
+
+        public void m(MyValue1 v) {
+        }
+    }
+    
+    static value class MyValue4 implements I {
+        int intField1;
+        int intField2;
+
+        MyValue4(int intField1, int intField2) {
+            this.intField1 = intField1;
+            this.intField2 = intField2;
+        }
+
+        public void m(MyValue1 v) {
+        }
+    }
+}
