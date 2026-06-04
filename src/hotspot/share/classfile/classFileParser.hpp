1e025c1bec54233001b2dd99f2d84c8bbf9bd6b8
Matias Saavedra Silva <matias.s.saavedra.silva@oracle.com>
2026-06-04 10:47:26 -0400

    Dan comment

--- src/hotspot/share/classfile/classFileParser.hpp
+++ src/hotspot/share/classfile/classFileParser.hpp
@@ -576,7 +576,6 @@ class ClassFileParser {
   bool is_inline_type() const { return !_access_flags.is_identity_class() && !_access_flags.is_interface() && !_access_flags.is_abstract(); }
   bool is_identity_class() const { return _access_flags.is_identity_class(); }
   bool has_inlined_fields() const { return _layout_info->_has_inlined_fields; }
-  bool has_static_null_restricted_fields() const { return _has_null_restricted_static_fields; }
 
   u2 java_fields_count() const { return _java_fields_count; }
   bool is_abstract() const { return _access_flags.is_abstract(); }
